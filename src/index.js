10c9ca5b6273cc8ba7f0bf275e0a1b64a13843ad
Thor Amorim <thoramorim@gmail.com>
2016-06-10 16:45:05 -0300

    Implement WorkspaceSandboxes calls

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import AppsClient from './AppsClient.js';
 import SandboxesClient from './SandboxesClient.js';
 import WorkspaceAppsClient from './WorkspaceAppsClient.js';
+import WorkspaceSandboxesClient from './WorkspaceSandboxesClient.js';
 import getEndpointUrl from './utils/appsEndpoints.js';
 
 module.exports = {
   AppsClient,
   SandboxesClient,
   WorkspaceAppsClient,
+  WorkspaceSandboxesClient,
   getEndpointUrl
 };
