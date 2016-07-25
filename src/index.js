4c7bdc568327b97d9123d684c4fdbe4e6d380494
Thor Amorim <thoramorim@gmail.com>
2016-07-25 18:38:24 -0300

    Add RegistryClient

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import AppsClient from './AppsClient.js';
+import RegistryClient from './RegistryClient.js';
 import getEndpointUrl from './utils/appsEndpoints.js';
 
 module.exports = {
   AppsClient,
+  RegistryClient,
   getEndpointUrl
 };
