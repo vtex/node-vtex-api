748daa3a10f6b70147d58f62e77d99b42b1ef718
Guilherme Rodrigues <gadr90@gmail.com>
2016-10-03 16:16:47 -0300

    Remove unused http and required

--- src/utils/required.js
+++
@@ -1,7 +0,0 @@
-export default function checkRequiredParameters (params) {
-  for (let key in params) {
-    if (params[key] === undefined) {
-      throw new Error(`${key} is a required attribute`)
-    }
-  }
-}
