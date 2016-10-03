04184d4d3003b2092a535b561507c024c3cfdebf
Guilherme Rodrigues <gadr90@gmail.com>
2016-10-03 17:16:32 -0300

    Reimplement apps client

--- src/AppsClient.js
+++ src/AppsClient.js
@@ -1,151 +1,104 @@
-import request from './http'
-import getEndpointUrl from './utils/apiEndpoints.js'
-import checkRequiredParameters from './utils/required.js'
+/* @flow */
+import Client from './Client'
+import {api} from './endpoints'
 
-class AppsClient {
-  constructor ({authToken, userAgent, endpointUrl = getEndpointUrl('STABLE')}) {
-    checkRequiredParameters({authToken, userAgent})
-    this.authToken = authToken
-    this.endpointUrl = endpointUrl === 'BETA'
-      ? getEndpointUrl(endpointUrl)
-      : endpointUrl
-    this.userAgent = userAgent
-    this.headers = {
-      authorization: `token ${this.authToken}`,
-      'user-agent': this.userAgent,
-    }
-    this.http = request.defaults({
-      headers: this.headers,
-    })
+const routes = {
+  Apps: (account, workspace) =>
+    `/${account}/${workspace}/apps`,
+
+  App: (account, workspace, app) =>
+    `${this.Apps(account, workspace)}/${app}`,
+
+  Settings: (account, workspace, app) =>
+    `${this.App(account, workspace, app)}/settings`,
+
+  Files: (account, workspace, app) =>
+    `${this.App(account, workspace, app)}/files`,
+
+  File: (account, workspace, app, path) =>
+    `${this.Files(account, workspace, app)}/${path}`,
+
+  DependencyMap: (account, workspace) =>
+    `/${account}/${workspace}/dependencyMap`,
+}
+
+const contextQuery = (context?: array) => context ? context.join('/') : context
+
+export default class AppsClient extends Client {
+  constructor (authToken: string, userAgent: string, endpointUrl: string = 'STABLE') {
+    super(authToken, userAgent, api(endpointUrl))
+    this.routes = routes
   }
 
-  installApp (account, workspace, descriptor) {
-    checkRequiredParameters({account, workspace, descriptor})
-    const url = `${this.endpointUrl}${this.routes.Apps(account, workspace)}`
-
-    return this.http.post(url).send(descriptor).thenJson()
+  installApp (account: string, workspace: string, descriptor: string) {
+    return this.http.post(this.routes.Apps(account, workspace), descriptor)
   }
 
-  uninstallApp (account, workspace, app) {
-    checkRequiredParameters({account, workspace, app})
-    const url = `${this.endpointUrl}${this.routes.App(account, workspace, app)}`
-
-    return this.http.delete(url).thenJson()
+  uninstallApp (account: string, workspace: string, app: string) {
+    return this.http.delete(this.routes.App(account, workspace, app))
   }
 
-  getAppSettings (account, workspace, app, {context = []} = {}) {
-    checkRequiredParameters({account, workspace, app})
-    const url = `${this.endpointUrl}${this.routes.Settings(account, workspace, app)}`
-
-    return this.http.get(url).query({
-      context: contextQuery(context),
-    }).thenJson()
+  getAppSettings (account: string, workspace: string, app: string, {context} = {}) {
+    const params = {context: contextQuery(context)}
+    return this.http(
+      this.routes.Settings(account, workspace, app),
+      {params},
+    )
   }
 
-  updateAppSettings (account, workspace, app, settings, {context = []} = {}) {
-    checkRequiredParameters({account, workspace, app, settings})
-    const url = `${this.endpointUrl}${this.routes.Settings(account, workspace, app)}`
-
-    return this.http.put(url).query({
-      context: contextQuery(context),
-    }).send(settings).thenJson()
+  updateAppSettings (account: string, workspace: string, app: string, settings: any, {context} = {}) {
+    const params = {context: contextQuery(context)}
+    return this.http.put(
+      this.routes.Settings(account, workspace, app),
+      settings,
+      {params},
+    )
   }
 
-  patchAppSettings (account, workspace, app, settings, {context = []} = {}) {
-    checkRequiredParameters({account, workspace, app, settings})
-    const url = `${this.endpointUrl}${this.routes.Settings(account, workspace, app)}`
-
-    return this.http.patch(url).query({
-      context: contextQuery(context),
-    }).send(settings).thenJson()
+  patchAppSettings (account: string, workspace: string, app: string, settings: any, {context} = {}) {
+    const params = {context: contextQuery(context)}
+    return this.http.patch(
+      this.routes.Settings(account, workspace, app),
+      settings,
+      {params},
+    )
   }
 
-  updateAppTtl (account, workspace, app) {
-    checkRequiredParameters({account, workspace, app})
-    const url = `${this.endpointUrl}${this.routes.App(account, workspace, app)}`
-
-    return this.http.patch(url).thenJson()
+  updateAppTtl (account: string, workspace: string, app: string) {
+    return this.http.patch(this.routes.App(account, workspace, app))
   }
 
-  listApps (account, workspace, options = {oldVersion: '', context: [], since: '', service: ''}) {
-    checkRequiredParameters({account, workspace})
-    const url = `${this.endpointUrl}${this.routes.Apps(account, workspace)}`
-    const {oldVersion, context, since, service} = options
-
-    return this.http.get(url).query({
+  listApps (account: string, workspace: string, {oldVersion, context, since, service}) {
+    const params = {
       oldVersion,
       context: contextQuery(context),
       since,
       service,
-    }).thenJson()
+    }
+    return this.http(this.routes.Apps(account, workspace), {params})
   }
 
-  listAppFiles (account, workspace, app, {prefix = '', context = [], nextMarker = ''}) {
-    checkRequiredParameters({account, workspace, app})
-    const url = `${this.endpointUrl}${this.routes.Files(account, workspace, app)}`
-    const marker = encodeURIComponent(nextMarker)
-
-    return this.http.get(url).query({
+  listAppFiles (account: string, workspace: string, app: string, {prefix, context, nextMarker}) {
+    const params = {
       prefix,
       context: contextQuery(context),
-      marker,
-    }).thenJson()
+      marker: encodeURIComponent(nextMarker),
+    }
+    return this.http(this.routes.Files(account, workspace, app), {params})
   }
 
-  getAppFile (account, workspace, app, path, context = []) {
-    checkRequiredParameters({account, workspace, app, path})
-    const url = `${this.endpointUrl}${this.routes.File(account, workspace, app, path)}`
-
-    return this.http.get(url).query({
-      context: contextQuery(context),
-    }).thenText()
+  getAppFile (account: string, workspace: string, app: string, path: string, context: array = []) {
+    const params = {context: contextQuery(context)}
+    return this.http(this.routes.File(account, workspace, app, path), {params})
   }
 
-  getApp (account, workspace, app, context = []) {
-    checkRequiredParameters({account, workspace, app})
-    const url = `${this.endpointUrl}${this.routes.App(account, workspace, app)}`
-
-    return this.http.get(url).query({
-      context: contextQuery(context),
-    }).thenJson()
+  getApp (account: string, workspace: string, app: string, context: array = []) {
+    const params = {context: contextQuery(context)}
+    return this.http(this.routes.App(account, workspace, app), {params})
   }
 
-  getDependencyMap (account, workspace, service = '') {
-    checkRequiredParameters({account, workspace})
-    const url = `${this.endpointUrl}${this.routes.DependencyMap(account, workspace)}`
-
-    return this.http.get(url).query({
-      service,
-    }).thenJson()
+  getDependencyMap (account: string, workspace: string, service: string) {
+    const params = {service}
+    return this.http(this.routes.DependencyMap(account, workspace), {params})
   }
 }
-
-AppsClient.prototype.routes = {
-  Apps (account, workspace) {
-    return `/${account}/${workspace}/apps`
-  },
-
-  App (account, workspace, app) {
-    return `${this.Apps(account, workspace)}/${app}`
-  },
-
-  Settings (account, workspace, app) {
-    return `${this.App(account, workspace, app)}/settings`
-  },
-
-  Files (account, workspace, app) {
-    return `${this.App(account, workspace, app)}/files`
-  },
-
-  File (account, workspace, app, path) {
-    return `${this.Files(account, workspace, app)}/${path}`
-  },
-
-  DependencyMap (account, workspace) {
-    return `/${account}/${workspace}/dependencyMap`
-  },
-}
-
-const contextQuery = (context) => context ? context.join('/') : context
-
-export default AppsClient
