4a0071e5b6a6811fd10758b5e4ade00d7cabd9f0
Rodrigo Dumont <rlsdumont@gmail.com>
2016-12-14 17:47:24 -0200

    Export base client

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import AppsClient from './AppsClient.js'
 import RegistryClient from './RegistryClient.js'
 import VBaseClient from './VBaseClient.js'
 import VTEXIDClient from './VTEXIDClient.js'
+import Client from './Client.js'
 
 module.exports = {
+  Client,
   AppsClient,
   RegistryClient,
   VBaseClient,
