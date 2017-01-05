22e9b7d3d6c1f909bb6ae9827e481387d17a7908
Guilherme Rodrigues <gadr90@gmail.com>
2017-01-05 18:17:57 -0200

    Export all clients in index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import AppsClient from './AppsClient.js'
 import AppEngineClient from './AppEngineClient.js'
+import AppRegistryClient from './AppRegistryClient.js'
+import AppsClient from './AppsClient.js'
+import Client from './Client.js'
 import RegistryClient from './RegistryClient.js'
+import RouterClient from './RouterClient.js'
 import VBaseClient from './VBaseClient.js'
 import VTEXIDClient from './VTEXIDClient.js'
-import Client from './Client.js'
+import WorkspacesClient from './WorkspacesClient.js'
 
 module.exports = {
-  Client,
-  AppsClient,
   AppEngineClient,
+  AppRegistryClient,
+  AppsClient,
+  Client,
   RegistryClient,
+  RouterClient,
   VBaseClient,
   VTEXIDClient,
+  WorkspacesClient,
 }
