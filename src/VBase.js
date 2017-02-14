fb85034eb435ae29bc8e3933d7a0951717e5c5d3
amoreira <anderson.moreira@vtex.com.br>
2017-02-14 16:51:39 -0200

    Hotfix: Fix typo

--- src/VBase.js
+++ src/VBase.js
@@ -16,7 +16,7 @@ const routes = {
     `${routes.Bucket(bucket)}/files`,
 
   File: (bucket: string, path: string) =>
-    `${routes.Bucket(bucket)}/files$/${path}`,
+    `${routes.Bucket(bucket)}/files/${path}`,
 }
 
 export type VBaseInstance = {
