92641e24586dd28e38392adc26d84e380bbdf87b
José Iago Pereira Araújo <joseiago.p.a@gmail.com>
2017-10-30 17:29:55 -0200

    Adding querystring parameters on request

--- src/Colossus.ts
+++ src/Colossus.ts
@@ -1,12 +1,8 @@
 import {HttpClient, InstanceOptions} from './HttpClient'
 
-const getSubjectToRequest = (subject: string) => {
-  return subject.length > 0 ? `&subject=${subject}` : ""
-}
-
 const routes = {
-  Event: (subject: string, route: string) => `/events/${route}${getSubjectToRequest(subject)}`,
-  Log: (subject: string, level: string) => `/logs/${level}${getSubjectToRequest(subject)}`,
+  Event: (subject: string, route: string) => `/events/${route}`,
+  Log: (subject: string, level: string) => `/logs/${level}`,
 }
 
 export class Colossus {
@@ -17,10 +13,10 @@ export class Colossus {
   }
 
   sendLog = (subject: string, message: any, level: string) => {
-    return this.http.put(routes.Log(subject, level), message)
+    return this.http.put(routes.Log(subject, level), message, {params: {subject}})
   }
 
   sendEvent = (subject: string, route: string, message?: any) => {
-    return this.http.put(routes.Event(subject, route), message)
+    return this.http.put(routes.Event(subject, route), message, {params: {subject}})
   }
 }
