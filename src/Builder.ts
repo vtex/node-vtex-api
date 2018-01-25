b1e68f4967cda93dd6d4f7c9d961c305b4b88472
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-01-25 12:04:03 -0200

    Support sticky hosts

--- src/Builder.ts
+++ src/Builder.ts
@@ -16,30 +16,37 @@ const routes = {
 
 export class Builder {
   private http: HttpClient
+  private stickyHost: string
 
   constructor (opts: InstanceOptions) {
     this.http = HttpClient.forWorkspace('builder-hub.vtex', opts)
   }
 
   public publishApp = (app: string, files: File[], tag?: string) => {
-    return this.zipAndSend(routes.Publish(app), app, files, tag)
+    return this.zipAndSend(routes.Publish(app), app, files, {tag})
   }
 
   public linkApp = (app: string, files: File[]) => {
-    return this.zipAndSend(routes.Link(app), app, files)
+    return this.zipAndSend(routes.Link(app), app, files, {sticky: true})
   }
 
   public relinkApp = (app: string, changes: Change[]) => {
-    const headers = {'Content-Type': 'application/json'}
+    const headers = {
+      'Content-Type': 'application/json',
+      ...this.stickyHost && {'X-Vtex-Sticky-Host': this.stickyHost},
+    }
     return this.http.put<BuildResult>(routes.Relink(app), changes, {headers})
   }
 
   public clean = (app: string) => {
-    const headers = {'Content-Type': 'application/json'}
+    const headers = {
+      'Content-Type': 'application/json',
+      ...this.stickyHost && {'X-Vtex-Sticky-Host': this.stickyHost},
+    }
     return this.http.post<BuildResult>(routes.Clean(app), {headers})
   }
 
-  private zipAndSend = async (route: string, app: string, files: File[], tag?: string) => {
+  private zipAndSend = async (route: string, app: string, files: File[], {tag, sticky}: zipOptions = {}) => {
     if (!(files[0] && files[0].path && files[0].content)) {
       throw new Error('Argument files must be an array of {path, content}, where content can be a String, a Buffer or a ReadableStream.')
     }
@@ -48,17 +55,27 @@ export class Builder {
       throw new Error('No manifest.json file found in files.')
     }
     const zip = archiver('zip')
-    const request = this.http.post<BuildResult>(route, zip, {
+    const request = this.http.postRaw<BuildResult>(route, zip, {
       params: tag ? {tag} : EMPTY_OBJECT,
-      headers: {'Content-Type': 'application/octet-stream'},
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        ...sticky && {'X-Vtex-Sticky-Host': this.stickyHost || 'Request'},
+      },
     })
 
     files.forEach(({content, path}) => zip.append(content, {name: path}))
     const finalize = zip.finalize()
 
     const [response] = await Promise.all([request, finalize])
-    return response
+    const {data, headers: {'X-Vtex-Sticky-Host': host}} = response
+    this.stickyHost = host
+    return data
   }
+}
+
+type zipOptions = {
+  sticky?: boolean,
+  tag?: string,
 }
 
 export type BuildResult = {
