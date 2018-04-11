ace50f969ed0c457f83a0c1b869e62c5eb7ca90a
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-04-11 19:26:50 -0300

    Record x-vtex-meta for 404 responses

--- src/HttpClient/recorder.ts
+++ src/HttpClient/recorder.ts
@@ -4,6 +4,11 @@ export const addRecorderInterceptors = (http: AxiosInstance, recorder: Recorder)
   http.interceptors.response.use((response: AxiosResponse) => {
     recorder(response.headers)
     return response
+  }, (err: any) => {
+    if (err.response && err.response.headers && err.response.status === 404) {
+      recorder(err.response.headers)
+    }
+    return Promise.reject(err)
   })
 }
 
