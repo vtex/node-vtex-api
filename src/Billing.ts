8da39590d9da542315bc8415210a642d50f14961
artur.pimentel <artur.oliveira@vtex.com.br>
2018-07-10 16:21:30 -0300

    Create validation method

--- src/Billing.ts
+++ src/Billing.ts
@@ -2,6 +2,7 @@ import { HttpClient, InstanceOptions, IOContext } from './HttpClient'
 
 const routes = {
   contractStatus: '/_v/contractStatus',
+  validate: '/_v/validate',
 }
 
 export class Billing {
@@ -11,8 +12,12 @@ export class Billing {
     this.http = HttpClient.forWorkspace('billing.vtex', ioContext, opts)
   }
 
-  contractStatus = () => {
+  contractStatus = async () => {
     return this.http.get<ContractStatus>(routes.contractStatus)
+  }
+
+  validateBillingOptions = async (billingOptions: BillingOptions) => {
+    return this.http.post<void | ValidationError>(routes.validate, billingOptions)
   }
 }
 
@@ -20,4 +25,55 @@ export enum ContractStatus {
   ACTIVE = 'active_contract',
   INACTIVE = 'inactive_contract',
   NO_CONTRACT = 'no_contract',
+}
+
+export type ValidationError = {
+  error: {
+    message: string
+    name: string,
+  },
+}
+
+export interface BillingOptions {
+  free?: boolean
+  policies?: Policy[]
+  termsURL: string
+  support: Support
+}
+
+export interface Support {
+  email?: string
+  url?: string
+}
+
+export interface Policy {
+  plan: string
+  currency: string
+  billing: Billing
+}
+
+export interface Billing {
+  taxClassification: string
+  invoiceProvider: string
+  items: CalculationItem[]
+}
+
+export interface CalculationItem {
+  itemCurrency: string
+  fixed: number
+  calculatedByMetricUnit: CalculatedByMetricUnit
+}
+
+export interface CalculatedByMetricUnit {
+  metricId: string
+  metricName: string
+  minChargeValue: number
+  ranges: Range[]
+  route: string
+}
+
+export interface Range {
+  exclusiveFrom: number
+  inclusiveTo: number
+  multiplier: number
 }
