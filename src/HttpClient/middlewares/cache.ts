363ba57a85dcbae154befbff9d066e199bb7a86b
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-09-03 17:44:56 -0300

    Add max-age caching support
    This is a naive implementation while we are looking for a new HTTP request library with native caching support.
    
    It's not possible to interrupt an axios request and respond from cache using interceptors. So this PR drops almost all interceptors in favor of koa-style middlewares. That will also make our future migration away from axios easier.

---
+++ src/HttpClient/middlewares/cache.ts
@@ -0,0 +1,103 @@
+import {AxiosRequestConfig, AxiosResponse} from 'axios'
+import {URL, URLSearchParams} from 'url'
+import {MiddlewareContext} from '../context'
+
+const cacheKey = (config: AxiosRequestConfig) => {
+  const {baseURL = '', url = '', params} = config
+  const fullURL = [baseURL, url].filter(str => str).join('/')
+  const urlObject = new URL(fullURL)
+  const searchParams = new URLSearchParams(params)
+  urlObject.search = searchParams.toString()
+  return urlObject.toString()
+}
+
+const parseCacheControl = (headers: Record<string, string>) => {
+  const {'cache-control': cacheControl = ''} = headers
+  const cacheDirectives = cacheControl.split(',').map(d => d.trim())
+  const maxAgeDirective = cacheDirectives.find(d => d.startsWith('max-age'))
+  const [, maxAgeStr] = maxAgeDirective ? maxAgeDirective.split('=') : [null, null]
+  const maxAge = maxAgeStr ? parseInt(maxAgeStr, 10) : 0
+
+  return {
+    maxAge,
+    noCache: cacheDirectives.indexOf('no-cache') !== -1,
+    noStore: cacheDirectives.indexOf('no-store') !== -1,
+  }
+}
+
+function isCacheable (arg: any): arg is CacheableRequestConfig {
+  return arg && arg.cacheable
+}
+
+const addNotModified = (validateStatus: (status: number) => boolean) =>
+  (status: number) => validateStatus(status) || status === 304
+
+export const cacheMiddleware = (cacheStorage: CacheStorage) => {
+  return async (ctx: MiddlewareContext, next: () => Promise<void>) => {
+    if (!isCacheable(ctx.config)) {
+      return await next()
+    }
+
+    const key = cacheKey(ctx.config)
+    const cached = cacheStorage.get<Cached>(key)
+
+    if (cached) {
+      const {etag, response, expiration} = cached
+      if (expiration > Date.now()) {
+        ctx.response = response
+        return
+      }
+
+      const validateStatus = addNotModified(ctx.config.validateStatus!)
+      if (etag && validateStatus(response.status)) {
+        ctx.config.headers['if-none-match'] = etag
+        ctx.config.validateStatus = validateStatus
+      }
+    }
+
+    await next()
+
+    const {response} = ctx
+    if (!response) {
+      return
+    }
+
+    if (response.status === 304) {
+      ctx.response = {...cached!.response, status: 304}
+      return
+    }
+
+    const {headers} = response
+    const {etag} = headers
+    const {maxAge, noStore} = parseCacheControl(headers)
+    if (noStore && !etag) {
+      return
+    }
+
+    if (maxAge || etag) {
+      const {status, data} = response
+      cacheStorage.set(key, {
+        etag,
+        response: {data, headers, status},
+        expiration: Date.now() + 1000 * maxAge,
+      })
+      return
+    }
+  }
+}
+
+interface Cached {
+  etag: string
+  expiration: number
+  response: AxiosResponse
+}
+
+export interface CacheStorage {
+  get<T> (key: string): T | undefined
+  set (key: string, value: any): void
+}
+
+export type CacheableRequestConfig = AxiosRequestConfig & {
+  url: string,
+  cacheable: boolean,
+}
---
+++ src/HttpClient/middlewares/notFound.ts
@@ -0,0 +1,31 @@
+import {AxiosRequestConfig} from 'axios'
+import {MiddlewareContext} from '../context'
+
+const addNotFound = (validateStatus: (status: number) => boolean) =>
+  (status: number) => validateStatus(status) || status === 404
+
+function nullIfNotFound (config: any): boolean {
+  return config && config.nullIfNotFound
+}
+
+export const acceptNotFoundMiddleware = async (ctx: MiddlewareContext, next: () => Promise<void>) => {
+  const {config} = ctx
+  if (nullIfNotFound(config)) {
+    ctx.config.validateStatus = addNotFound(config.validateStatus!)
+  }
+
+  await next()
+}
+
+export const notFoundFallbackMiddleware = async (ctx: MiddlewareContext, next: () => Promise<void>) => {
+  await next()
+
+  const {config} = ctx
+  if (nullIfNotFound(config) && ctx.response) {
+    ctx.response.data = null
+  }
+}
+
+export type IgnoreNotFoundRequestConfig = AxiosRequestConfig & {
+  nullIfNotFound?: boolean,
+}
