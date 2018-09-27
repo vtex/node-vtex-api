4cbe0514f58fb750b7bf42a7afe7bc2b1794a584
pedroig <pedroigorao@hotmail.com>
2018-09-27 14:29:26 -0300

    making Metrics Accumulator more extendable

--- src/MetricsAccumulator.ts
+++ src/MetricsAccumulator.ts
@@ -1,4 +1,4 @@
-import {flatten, map, mapObjIndexed, values} from 'ramda'
+import {filter, flatten, map, mapObjIndexed, values} from 'ramda'
 import {mean, median, percentile} from 'stats-lite'
 
 import {hrToMillis} from './utils/Time'
@@ -8,13 +8,17 @@ interface Metric {
   [key: string]: number | boolean | string | undefined
 }
 
-interface AggregateMetric extends Metric {
+interface AggregateMetric {
   count: number
+  max: number
   mean: number
   median: number
+  min: number
   percentile95: number
   percentile99: number
-  max: number
+}
+
+interface EnrichedAggregateMetric extends Metric, AggregateMetric {
   production: boolean
 }
 
@@ -35,39 +39,20 @@ function cpuUsage () {
   return diff
 }
 
-const createMetricToAggregateReducer = (production: boolean) => (value: any, key: string, obj: any): AggregateMetric => {
-  const aggregate: AggregateMetric = {
-    name: key,
-    count: value.length, // tslint:disable-line:object-literal-sort-keys
-    max: Math.max(...value),
-    mean: mean(value),
-    median: median(value),
-    percentile95: percentile(value, 0.95),
-    percentile99: percentile(value, 0.99),
-    production,
-  }
-  delete obj[key]
-  return aggregate
-}
-
 export class MetricsAccumulator {
   // Metrics from production workspaces
-  private metricsMillis: Record<string, number[]>
+  private metricsMillis: Record<string, number[]> = {}
   // Metrics from development workspaces
-  private devMetricsMillis: Record<string, number[]>
+  private devMetricsMillis: Record<string, number[]> = {}
   // Tracked cache instances
   private cacheMap: Record<string, GetStats>
-
+  
   private onFlushMetrics: Array<() => Metric | Metric[]>
-
-  private metricToAggregate = createMetricToAggregateReducer(true)
-  private devMetricToAggregate = createMetricToAggregateReducer(false)
-
+  
   constructor() {
-    this.metricsMillis = {}
-    this.devMetricsMillis = {}
     this.onFlushMetrics = []
     this.cacheMap = {}
+    this.resetAccumulators()
   }
 
   public batchHrTimeMetricFromEnd = (name: string, end: [number, number], production: boolean) => {
@@ -104,21 +89,50 @@ export class MetricsAccumulator {
     return this.flushMetrics()
   }
 
-  private cacheToMetric = (value: GetStats, key: string): Metric => ({
-    name: `${key}-cache`,
-    ...value.getStats(),
+  protected createMetricToAggregateReducer = (production: boolean) => (value: any, key: string, obj: any): EnrichedAggregateMetric => { //must be protected in order to override
+    const metricToAggregateReducer: EnrichedAggregateMetric = {
+      name: key,
+      production,
+      ...this.aggregate(value),
+    }
+    return metricToAggregateReducer
+  }
+  
+  protected aggregate = (value: number[]): AggregateMetric => ({
+    count: value.length, // tslint:disable-line:object-literal-sort-keys
+    max: Math.max(...value),
+    mean: mean(value),
+    median: median(value),
+    min: Math.min(...value),
+    percentile95: percentile(value, 0.95),
+    percentile99: percentile(value, 0.99),
   })
 
-  private flushMetrics = (): Metric[] => {
-    const aggregateMetrics: Metric[] = values(mapObjIndexed(
-      this.metricToAggregate,
+  protected resetAccumulators = () => {
+    this.metricsMillis = {}
+    this.devMetricsMillis = {}
+  }
+
+  protected getAggregateMetrics = (): Metric[] => {
+    const removeIrrelevantMetrics = (metrics: EnrichedAggregateMetric[]): Metric[] => 
+      filter(metric => metric.max > 10, metrics)
+
+    const aggregateMetricsAll: EnrichedAggregateMetric[] = values(mapObjIndexed(
+      this.createMetricToAggregateReducer(true),
       this.metricsMillis,
     ))
+    const aggregateMetrics = removeIrrelevantMetrics(aggregateMetricsAll)
 
-    const aggregateDevMetrics: Metric[] = values(mapObjIndexed(
-      this.devMetricToAggregate,
+    const aggregateDevMetricsAll: EnrichedAggregateMetric[] = values(mapObjIndexed(
+      this.createMetricToAggregateReducer(false),
       this.devMetricsMillis,
     ))
+    const aggregateDevMetrics = removeIrrelevantMetrics(aggregateDevMetricsAll)
+
+    return [...aggregateMetrics, ...aggregateDevMetrics]
+  }
+
+  protected flushMetrics = (): Metric[] => {      // must it be protected in order to override
 
     const systemMetrics: Metric[] = [
       {
@@ -138,6 +152,13 @@ export class MetricsAccumulator {
       this.cacheMap,
     ))
 
-    return [...systemMetrics, ...aggregateMetrics, ...aggregateDevMetrics, ...onFlushMetrics, ...cacheMetrics]
+    this.resetAccumulators()
+
+    return [...systemMetrics, ...this.getAggregateMetrics(), ...onFlushMetrics, ...cacheMetrics]
   }
+
+  private cacheToMetric = (value: GetStats, key: string): Metric => ({
+    name: `${key}-cache`,
+    ...value.getStats(),
+  })
 }
