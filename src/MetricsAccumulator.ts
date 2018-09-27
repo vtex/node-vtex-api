e823c78d896977fa6ba38e238cf4e521ded559f4
pedroig <pedroigorao@hotmail.com>
2018-09-27 14:29:26 -0300

    making Metrics Accumulator more extendable

--- src/MetricsAccumulator.ts
+++ src/MetricsAccumulator.ts
@@ -1,4 +1,4 @@
-import {flatten, map, mapObjIndexed, values} from 'ramda'
+import {filter, flatten, map, mapObjIndexed, values} from 'ramda'
 import {mean, median, percentile} from 'stats-lite'
 
 import {hrToMillis} from './utils/Time'
@@ -8,13 +8,17 @@ interface Metric {
   [key: string]: number | boolean | string | undefined
 }
 
-interface AggregateMetric extends Metric {
+interface AggregateMetric {
   count: number
+  max: number
   mean: number
   median: number
+  min: number
   percentile95: number
   percentile99: number
-  max: number
+}
+
+interface ContextMetric extends Metric, AggregateMetric {
   production: boolean
 }
 
@@ -24,50 +28,34 @@ interface GetStats {
   }
 }
 
-let lastCpu: NodeJS.CpuUsage = process.cpuUsage()
-
-function cpuUsage () {
-  const diff = process.cpuUsage(lastCpu)
-  lastCpu = {
-    system: lastCpu.system + diff.system,
-    user: lastCpu.user + diff.user,
-  }
-  return diff
-}
-
-const createMetricToAggregateReducer = (production: boolean) => (value: any, key: string, obj: any): AggregateMetric => {
-  const aggregate: AggregateMetric = {
-    name: key,
-    count: value.length, // tslint:disable-line:object-literal-sort-keys
-    max: Math.max(...value),
-    mean: mean(value),
-    median: median(value),
-    percentile95: percentile(value, 0.95),
-    percentile99: percentile(value, 0.99),
-    production,
-  }
-  delete obj[key]
-  return aggregate
-}
-
 export class MetricsAccumulator {
+  static lastCpu: NodeJS.CpuUsage = process.cpuUsage()
+
   // Metrics from production workspaces
-  private metricsMillis: Record<string, number[]>
+  private metricsMillis: Record<string, number[]> = {}
   // Metrics from development workspaces
-  private devMetricsMillis: Record<string, number[]>
+  private devMetricsMillis: Record<string, number[]> = {}
   // Tracked cache instances
   private cacheMap: Record<string, GetStats>
-
+  
   private onFlushMetrics: Array<() => Metric | Metric[]>
 
-  private metricToAggregate = createMetricToAggregateReducer(true)
-  private devMetricToAggregate = createMetricToAggregateReducer(false)
+  protected createMetricToAggregateReducer = (production: boolean) => (value: any, key: string, obj: any): EnrichedAggregateMetric => {
+    const metricToAggregateReducer: ContextMetric = {
+      name: key,
+      production,
+      ...this.aggregate(value),
+    }
+    return metricToAggregateReducer
+  }
 
+  private metricToAggregate = this.createMetricToAggregateReducer(true)
+  private devMetricToAggregate = this.createMetricToAggregateReducer(false)
+  
   constructor() {
-    this.metricsMillis = {}
-    this.devMetricsMillis = {}
     this.onFlushMetrics = []
     this.cacheMap = {}
+    this.resetAccumulators()
   }
 
   public batchHrTimeMetricFromEnd = (name: string, end: [number, number], production: boolean) => {
@@ -103,27 +91,53 @@ export class MetricsAccumulator {
   public statusTrack = () => {
     return this.flushMetrics()
   }
-
-  private cacheToMetric = (value: GetStats, key: string): Metric => ({
-    name: `${key}-cache`,
-    ...value.getStats(),
+  
+  protected aggregate = (value: number[]): AggregateMetric => ({
+    count: value.length, // tslint:disable-line:object-literal-sort-keys
+    max: Math.max(...value),
+    mean: mean(value),
+    median: median(value),
+    min: Math.min(...value),
+    percentile95: percentile(value, 0.95),
+    percentile99: percentile(value, 0.99),
   })
 
-  private flushMetrics = (): Metric[] => {
-    const aggregateMetrics: Metric[] = values(mapObjIndexed(
+  protected resetAccumulators = () => {
+    this.metricsMillis = {}
+    this.devMetricsMillis = {}
+  }
+
+  protected getAggregateMetrics = (): Metric[] => {
+    const aggregateMetricsAll: ContextMetric[] = values(mapObjIndexed(
       this.metricToAggregate,
       this.metricsMillis,
     ))
+    const aggregateMetrics = aggregateMetricsAll
 
-    const aggregateDevMetrics: Metric[] = values(mapObjIndexed(
+    const aggregateDevMetricsAll: ContextMetric[] = values(mapObjIndexed(
       this.devMetricToAggregate,
       this.devMetricsMillis,
     ))
+    const aggregateDevMetrics = aggregateDevMetricsAll
+
+    return [...aggregateMetrics, ...aggregateDevMetrics]
+  }
+
+  protected cpuUsage () {
+    const diff = process.cpuUsage(MetricsAccumulator.lastCpu)
+    MetricsAccumulator.lastCpu = {
+      system: MetricsAccumulator.lastCpu.system + diff.system,
+      user: MetricsAccumulator.lastCpu.user + diff.user,
+    }
+    return diff
+  }
+
+  protected flushMetrics = (): Metric[] => {
 
     const systemMetrics: Metric[] = [
       {
         name: 'cpu',
-        ...cpuUsage(),
+        ...this.cpuUsage(),
       },
       {
         name: 'memory',
@@ -138,6 +152,13 @@ export class MetricsAccumulator {
       this.cacheMap,
     ))
 
-    return [...systemMetrics, ...aggregateMetrics, ...aggregateDevMetrics, ...onFlushMetrics, ...cacheMetrics]
+    this.resetAccumulators()
+
+    return [...systemMetrics, ...this.getAggregateMetrics(), ...onFlushMetrics, ...cacheMetrics]
   }
+
+  private cacheToMetric = (value: GetStats, key: string): Metric => ({
+    name: `${key}-cache`,
+    ...value.getStats(),
+  })
 }
