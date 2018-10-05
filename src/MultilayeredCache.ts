91b96fef5489a8d9d5850eff8bb1fe36ca0be5f1
pedroig <pedroigorao@hotmail.com>
2018-10-05 20:29:03 -0300

    added MultilayerStats

--- src/MultilayeredCache.ts
+++ src/MultilayeredCache.ts
@@ -1,33 +1,26 @@
-import { slice } from 'ramda'
-import { Stats } from './LRUCache'
-
-const findIndex = async <T> (func: (item: T) => Promise<boolean>, array: T[]): Promise<number> => {
-  for(let index=0; index < array.length; index++) {
-    const hasKey = await func(array[index])
-    if (hasKey) {
-      return index
-    }
-  }
-  return -1
-}
+import { map, slice } from 'ramda'
 
 export class MultilayeredCache <K, V> implements CacheLayer<K, V>{
 
-  constructor(private caches: Array<CacheLayer<K, V>>) {
-  }
+  private hits = 0
+  private total = 0
+
+  constructor(private caches: Array<CacheLayer<K, V>>) {}
 
   public get = async (key: K, fetcher?: () => V): Promise<V | void> => {
     let value: V | void
-    let successIndex = await findIndex(async (cache: CacheLayer<K, V>) => {
+    let successIndex = await this.findIndex(async (cache: CacheLayer<K, V>) => {
       const [getValue, hasKey] = await Promise.all([cache.get(key), cache.has(key)])
       value = getValue
       return hasKey
     },this.caches)
     if (successIndex === -1) {
-      if (fetcher) { 
-        //
+      if (fetcher) {
+        value = fetcher()
       }
-      value = undefined //
+      else {
+        return undefined
+      }
       successIndex = Infinity
     }
     const failedCaches = slice(0, successIndex, this.caches)
@@ -48,12 +41,51 @@ export class MultilayeredCache <K, V> implements CacheLayer<K, V>{
     })
     return hasInAtLeastOneCache
   }
-}
 
+  public getStats = async (): Promise<MultilayerStats> => {
+    const layersStats = Promise.all(map(
+      (cache: CacheLayer<K, V>) => cache.getStats
+        ? cache.getStats()
+        : undefined
+      , this.caches))
+    const multilayerStats =  {
+      hitRate: this.total > 0 ? this.hits / this.total : undefined,
+      hits: this.hits,
+      layers: layersStats,
+      total: this.total,
+    }
+    this.resetCounters()
+    return multilayerStats
+  }
+
+  private findIndex = async <T> (func: (item: T) => Promise<boolean>, array: T[]): Promise<number> => {
+    this.total += 1
+    for(let index=0; index < array.length; index++) {
+      const hasKey = await func(array[index])
+      if (hasKey) {
+        this.hits += 1
+        return index
+      }
+    }
+    return -1
+  }
+
+  private resetCounters() {
+    this.hits = 0
+    this.total = 0
+  }
+}
 
 export interface CacheLayer<K, V> {
   get(key: K, fetcher?: () => V): Promise<V | void>,
   has(key: K): Promise<boolean>,
   set(key: K, value: V): void,
-  getStats?(): Stats,
+  getStats?(): any,
+}
+
+export interface MultilayerStats {
+  hitRate: number | undefined,
+  hits: number,
+  total: number,
+  layers: any
 }
