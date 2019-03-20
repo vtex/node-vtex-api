bebdf4f4846a199e8f4cfd6736a0bcbfbcbc6317
artur.pimentel <artur.oliveira@vtex.com.br>
2019-03-20 14:24:29 -0300

    Add BillingOptions typing utils

---
+++ src/utils/billingOptions.ts
@@ -0,0 +1,9 @@
+import { isNil } from 'ramda'
+
+import { BillingOptions, CalculationItem, FixedCalculationItem, FreeBillingOptions } from '../responses'
+
+export const isFreeBillingOptions = (billingOptions: BillingOptions): billingOptions is FreeBillingOptions =>
+  (billingOptions as FreeBillingOptions).free
+
+export const isFixedCalculationItem = (item: CalculationItem): item is FixedCalculationItem =>
+  !isNil((item as FixedCalculationItem).fixed)
