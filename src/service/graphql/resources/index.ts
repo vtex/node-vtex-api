81cd3704bdbdaa92369939d091df820d9872d8e8
Guilherme Rodrigues <gadr90@gmail.com>
2019-03-22 18:04:02 -0300

    WIP

---
+++ src/service/graphql/resources/index.ts
@@ -0,0 +1,45 @@
+import { Cached, isNetworkErrorOrRouterTimeout, LRUCache, ServiceContext } from '@vtex/api'
+import * as DataLoader from 'dataloader'
+
+import { IOMessage } from '../schema/typeDefs/ioMessage'
+import { MessagesAPI, messagesLoader } from './messages'
+import { SegmentAPI } from './segment'
+
+const cacheStorage = new LRUCache<string, Cached>({
+  max: 2000,
+})
+
+if (global.metrics) {
+  global.metrics.trackCache('runtime-segment', cacheStorage)
+}
+
+// Retry on timeout from our end
+const isAborted = (e: any) => {
+  if (e && e.code === 'ECONNABORTED') {
+    return true
+  }
+  return isNetworkErrorOrRouterTimeout(e)
+}
+
+const TWO_SECONDS_MS =  2 * 1000
+const TEN_SECONDS_MS = 10 * 1000
+const retryConfig = {
+  retries: 1,
+  retryCondition: isAborted,
+  shouldResetTimeout: true,
+}
+
+export class Resources {
+  public messagesAPI: MessagesAPI
+  public segmentAPI: SegmentAPI
+  public translationsLoader: DataLoader<IOMessage, string>
+
+  constructor (ctx: ServiceContext) {
+    const {vtex} = ctx
+    const metrics = global.metrics
+
+    this.segmentAPI = new SegmentAPI(vtex, {memoryCache: cacheStorage, timeout: TWO_SECONDS_MS, retryConfig, metrics}, ctx.clients.logger)
+    this.messagesAPI = new MessagesAPI(vtex, {timeout: TEN_SECONDS_MS, retryConfig, metrics}, ctx.clients.logger)
+    this.translationsLoader = messagesLoader({messagesAPI: this.messagesAPI, segmentAPI: this.segmentAPI, logger: ctx.clients.logger})
+  }
+}
---
+++ src/service/graphql/resources/segment.ts
@@ -0,0 +1,62 @@
+import {
+  HttpClient,
+  HttpClientFactory,
+  InstanceOptions,
+  IOContext,
+  IODataSource,
+  Logger,
+} from '@vtex/api'
+import { prop } from 'ramda'
+
+const factory: HttpClientFactory = ({context, options}) => context && 
+  HttpClient.forExternal(`http://portal.vtexcommercestable.com.br`, context, options || {})
+
+interface Segment {
+  channel: number,
+  utm_campaign: string,
+  utm_source: string,
+  utmi_campaign: string,
+  currencyCode: string,
+  currencySymbol: string,
+  countryCode: string,
+  cultureInfo: string
+}
+
+const routes = {
+  segments: (token: string | undefined) => token ? `/api/segments/${token}` : '/api/segments',
+}
+
+export class SegmentAPI extends IODataSource{
+  protected memoizedResults: Record<string, Promise<Segment | null>> 
+  protected httpClientFactory = factory
+
+  constructor(ctx: IOContext, opts: InstanceOptions, protected logger: Logger) {
+    super(ctx, opts)
+    this.memoizedResults = {}
+  }
+
+  public segment = () => {
+    const {segmentToken, authToken, account} = this.context!
+    const key = segmentToken + account
+
+    if (!this.memoizedResults[key]) {
+      this.memoizedResults[key] = this.http.get<Segment>(routes.segments(segmentToken), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Proxy-Authorization': authToken,
+        },
+        metric: 'runtime-segment',
+        params: {
+          an: account,
+        },
+      }).catch(err => {
+        this.logger.error(err).catch(console.log)
+        return null
+      })
+    }
+
+    return this.memoizedResults[key]
+  }
+
+  public getTo = () => this.segment().then(res => res && prop('cultureInfo', res)) 
+}
