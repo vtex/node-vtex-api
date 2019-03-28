f0b965d20cebc34e2531e272b4c11d6f341e1fb6
Guilherme Rodrigues <gadr90@gmail.com>
2019-03-28 11:30:39 -0300

    Log details only on error

--- src/service/http/middlewares/timings.ts
+++ src/service/http/middlewares/timings.ts
@@ -11,54 +11,18 @@ const log = <T extends IOClients, U, V>(
 ) =>
   `${new Date().toISOString()}\t${account}/${workspace}:${id}\t${status}\t${method}\t${path}\t${millis}ms`
 
-
 export async function timings<T extends IOClients, U, V> (ctx: ServiceContext<T, U, V>, next: () => Promise<any>) {
   const start = process.hrtime()
-  let error
 
   updateLastLogger(ctx.clients.logger)
 
-  try {
-    await next()
-  } catch (e) {
-    error = e
-  } finally {
-    const {
-      method,
-      status,
-      vtex: {
-        operationId,
-        requestId,
-        route: {
-          id,
-        },
-      },
-      headers: {
-        'x-forwarded-path': forwardedPath,
-        'x-forwarded-host': forwardedHost,
-        'x-forwarded-proto': forwardedProto,
-        'x-vtex-platform': platform,
-      },
-    } = ctx
-    const end = process.hrtime(start)
-    const millis = hrToMillis(end)
-    const logType = error ? 'error' : 'info'
+  // Errors will be caught by the next middleware so we don't have to catch.
+  await next()
 
-    ctx.clients.logger.sendLog('-', {
-      ... error ? error : null,
-      forwardedHost,
-      forwardedPath,
-      forwardedProto,
-      method,
-      millis,
-      operationId,
-      platform,
-      requestId,
-      routeId: id,
-      status,
-    }, logType)
+  const { status, vtex: { route: { id } } } = ctx
+  const end = process.hrtime(start)
+  const millis = hrToMillis(end)
 
-    metrics.batch(`http-handler-${statusLabel(status)}-${id}`, end)
-    console.log(log(ctx, millis))
-  }
+  metrics.batch(`http-handler-${statusLabel(status)}-${id}`, end)
+  console.log(log(ctx, millis))
 }
