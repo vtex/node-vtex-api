974fdbdcc91afa8ef1c45917fbd73196f6a5531a
Tiago Gimenes <tlgimenes@gmail.com>
2019-04-18 16:48:12 -0300

    cleaning checkout cookies

--- src/service/http/middlewares/setCookie.ts
+++ src/service/http/middlewares/setCookie.ts
@@ -1,10 +1,16 @@
-import { find } from 'ramda'
+import { compose, find, forEach, head, intersection, isEmpty, map, split } from 'ramda'
 
 import { IOClients } from '../../../clients/IOClients'
 import { ServiceContext } from '../../typings'
 
-const warnMessage = `Removing set-cookie from response since cache-control has as public scope.
-This can be a huge security risk. Please remove either the public scope or set-cookie from your response.`
+const BLACKLISTED_COOKIES = ['checkout.vtex.com']
+
+const warnMessage = (keys: string[]) => `Removing set-cookie from response since cache-control has as public scope.
+This can be a huge security risk. Please remove either the public scope or set-cookie from your response.
+Cookies dropped:
+
+  ${keys.join('\n\t')}
+`
 
 const findStr = (target: string, set: string[]) => find((a: string) => a.toLocaleLowerCase() === target, set)
 
@@ -14,18 +20,28 @@ const findScopeInCacheControl = (cacheControl: string | undefined) => {
   return scopePublic
 }
 
+const extractKeys = (setCookie: string[]) => map<string, string>(compose(head, split('=')), setCookie)
+
 export async function removeSetCookie<T extends IOClients, U, V> (ctx: ServiceContext<T, U, V>, next: () => Promise<any>) {
+  const { clients: { logger } } = ctx
+
   await next()
 
-  const setCookie = ctx.response.headers['set-cookie']
-  if (!setCookie) {
+  const setCookie: string[] | undefined = ctx.response.headers['set-cookie']
+  if (!setCookie || isEmpty(setCookie)) {
     return
   }
 
-  const cacheControl = ctx.response.headers['cache-control']
+  const cacheControl: string | undefined = ctx.response.headers['cache-control']
   const scope = findScopeInCacheControl(cacheControl)
   if (scope === 'public') {
-    ctx.set('Set-Cookie', '')
-    console.warn(warnMessage)
+    const cookieKeys = extractKeys(setCookie)
+    const blacklist = intersection(BLACKLISTED_COOKIES, cookieKeys)
+    forEach<string, string[]>(cookie => ctx.cookies.set(cookie, undefined), blacklist)
+    console.warn(warnMessage(blacklist))
+    logger.warn({
+      cookieKeys,
+      message: 'Setting cookies in a public route!',
+    }).catch()
   }
 }
