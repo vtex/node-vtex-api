636d989f66ba9e564123b85e449b4ecf226abd05
Guilherme Rodrigues <gadr90@gmail.com>
2019-04-24 14:38:53 -0300

    Port LicenseManager Client

--- src/clients/LicenseManager.ts
+++ src/clients/LicenseManager.ts
@@ -1,8 +1,7 @@
 
 import { stringify } from 'qs'
 
-import { RequestConfig } from '../HttpClient/typings'
-import { forExternal, IODataSource } from '../IODataSource'
+import { JanusClient, RequestConfig } from '../HttpClient'
 
 const TWO_MINUTES_S = 2 * 60
 
@@ -15,40 +14,27 @@ const inflightKey = ({baseURL, url, params}: RequestConfig) => {
   return baseURL! + url! + stringify(params, {arrayFormat: 'repeat', addQueryPrefix: true})
 }
 
-export class LicenseManager extends IODataSource {
-  protected httpClientFactory = forExternal
-  protected service = 'http://portal.vtexcommercestable.com.br'
-
+export class LicenseManager extends JanusClient {
   public getAccountData () {
-    const {authToken, account} = this.context!
+    const {authToken} = this.context!
 
     return this.http.get(routes.accountData, {
       forceMaxAge: TWO_MINUTES_S,
       headers: {
-        'Proxy-Authorization': authToken,
         VtexIdclientAutCookie: authToken,
       },
       inflightKey,
       metric: 'lm-account-data',
-      params: {
-        an: account,
-      },
     })
   }
 
   public getTopbarData (VtexIdclientAutCookie: string) {
-    const {authToken, account} = this.context!
-
     return this.http.get(routes.topbarData, {
       headers: {
-        'Proxy-Authorization': authToken,
         VtexIdclientAutCookie,
       },
       memoizeable: false,
       metric: 'lm-topbar-data',
-      params: {
-        an: account,
-      },
     })
   }
 }
