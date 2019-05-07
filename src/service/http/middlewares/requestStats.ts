22dcd3827b0696d157c1f168d9ec2558b3cacbf0
Diego Ximenes Mendes <dxmendes1@gmail.com>
2019-05-07 16:28:17 -0300

    track aborted incoming msg

--- src/service/http/middlewares/requestStats.ts
+++ src/service/http/middlewares/requestStats.ts
@@ -2,17 +2,20 @@ import { IOClients } from '../../../clients/IOClients'
 import { ServiceContext } from '../../typings'
 
 class IncomingRequestStats {
+  public aborted = 0
   public closed = 0
   public total = 0
 
   public get () {
     return {
+      aborted: this.aborted,
       closed: this.closed,
       total: this.total,
     }
   }
 
   public clear () {
+    this.aborted = 0
     this.closed = 0
     this.total = 0
   }
@@ -23,9 +26,13 @@ export const incomingRequestStats = new IncomingRequestStats()
 const requestClosed = () => {
   incomingRequestStats.closed++
 }
+const requestAborted = () => {
+  incomingRequestStats.aborted++
+}
 
 export async function trackIncomingRequestStats <T extends IOClients, U, V> (ctx: ServiceContext<T, U, V>, next: () => Promise<any>) {
   ctx.req.on('close', requestClosed)
+  ctx.req.on('aborted', requestAborted)
   incomingRequestStats.total++
   await next()
 }
