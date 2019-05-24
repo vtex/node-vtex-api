224a312cd9c3d347d935b4a91b83f025bb0c26f5
Tiago Gimenes <tlgimenes@gmail.com>
2019-05-24 13:36:26 -0300

    adds etag control to smartcache directive

--- src/service/graphql/schema/schemaDirectives/SmartCacheDirective.ts
+++ src/service/graphql/schema/schemaDirectives/SmartCacheDirective.ts
@@ -1,10 +1,27 @@
 import { defaultFieldResolver, GraphQLField } from 'graphql'
 import { SchemaDirectiveVisitor } from 'graphql-tools'
 
+import { maxAgeEnums } from '../../utils/maxAgeEnum'
+
+const ETAG_CONTROL_HEADER = 'x-vtex-etag-control'
+
+interface Args {
+  maxAge: keyof typeof maxAgeEnums | undefined
+}
+
+const DEFAULT_ARGS: Args = {
+  maxAge: undefined,
+}
+
 export class SmartCacheDirective extends SchemaDirectiveVisitor {
   public visitFieldDefinition (field: GraphQLField<any, any>) {
     const {resolve = defaultFieldResolver} = field
+    const { maxAge } = this.args as Args || DEFAULT_ARGS
+    const maxAgeS = maxAge && maxAgeEnums[maxAge]
     field.resolve = (root, args, context, info) => {
+      if (maxAgeS) {
+        context.set(ETAG_CONTROL_HEADER, maxAgeS)
+      }
       context.vtex.recorder = context.state.recorder
       return resolve(root, args, context, info)
     }
