9ea6c9aecce13145c06d01e3f106f4e23c2b4f08
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-06-13 15:55:06 -0300

    Fixing...

--- src/service/http/middlewares/vary.ts
+++ src/service/http/middlewares/vary.ts
@@ -3,7 +3,7 @@ import { SEGMENT_HEADER, SESSION_HEADER } from '../../../constants'
 import { ServiceContext } from '../../typings'
 
 export async function vary <T extends IOClients, U, V> (ctx: ServiceContext<T, U, V>, next: () => Promise<any>) {
-  const { headers, method } = ctx
+  const { method } = ctx
 
   // We don't need to vary non GET requests, since they are never cached
   if (method.toUpperCase() !== 'GET') {
@@ -11,10 +11,10 @@ export async function vary <T extends IOClients, U, V> (ctx: ServiceContext<T, U
     return
   }
 
-  if (headers.get(SEGMENT_HEADER)) {
+  if (ctx.get(SEGMENT_HEADER)) {
     ctx.vary(SEGMENT_HEADER)
   }
-  if (headers.get(SESSION_HEADER)) {
+  if (ctx.get(SESSION_HEADER)) {
     ctx.vary(SESSION_HEADER)
   }
 
