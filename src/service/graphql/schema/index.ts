5daf27fce1e3da7eab53f4b713917cd9d192a8d3
Roger Leite Lucena <roger.lucena@polytechnique.edu>
2019-06-21 16:25:56 -0300

    Fix - problem with recorder inside context

--- src/service/graphql/schema/index.ts
+++ src/service/graphql/schema/index.ts
@@ -49,7 +49,7 @@ export const makeSchema = (ctx: GraphQLServiceContext) => {
 
   const resolverContext = {
     getLocaleTo,
-    translationsLoader: messagesLoader(ctx.clients.messagesGraphQL),
+    translationsLoader: messagesLoader(ctx.clients),
   }
 
   const executableSchema = makeExecutableSchema({
