37b58212b4b5327ed2738c334319009eddf430d6
Athos Couto <athos.couto@vtex.com.br>
2019-06-21 16:45:52 -0300

    Remove ramda call

--- src/service/graphql/middlewares/upload.ts
+++ src/service/graphql/middlewares/upload.ts
@@ -1,21 +1,20 @@
 import { graphqlUploadKoa } from 'graphql-upload'
-import { call } from 'ramda'
 import { GraphQLServiceContext } from '../typings'
 
 declare module 'graphql-upload' {
   export function graphqlUploadKoa(options: ApolloUploadOptions): (ctx: GraphQLServiceContext, next: () => Promise<any>) => Promise<void>
 }
 
-function graphqlUploadKoaMiddleware(ctx: GraphQLServiceContext, next: () => Promise<any>): Promise<void> {
-  return call(
-    graphqlUploadKoa({
-      maxFieldSize: 1e6, // size in Bytes
-      maxFileSize: 4 * 1e6, // size in Bytes
-      maxFiles: 5,
-    }),
-    ctx,
-    next
-  )
+function graphqlUploadKoaMiddleware(
+  ctx: GraphQLServiceContext,
+  next: () => Promise<any>
+): Promise<void> {
+  const graphqlUpload = graphqlUploadKoa({
+    maxFieldSize: 1e6, // size in Bytes
+    maxFileSize: 4 * 1e6, // size in Bytes
+    maxFiles: 5,
+  })
+  return graphqlUpload(ctx, next)
 }
 
 export const upload = graphqlUploadKoaMiddleware
