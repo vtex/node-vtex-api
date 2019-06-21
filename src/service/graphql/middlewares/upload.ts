f48870a55fb4900c1bee55d8133defe26da387b5
Athos Couto <athos.couto@vtex.com.br>
2019-06-21 12:11:33 -0300

    Name upload middleware

--- src/service/graphql/middlewares/upload.ts
+++ src/service/graphql/middlewares/upload.ts
@@ -1,12 +1,21 @@
 import { graphqlUploadKoa } from 'graphql-upload'
+import { call } from 'ramda'
 import { GraphQLServiceContext } from '../typings'
 
 declare module 'graphql-upload' {
   export function graphqlUploadKoa(options: ApolloUploadOptions): (ctx: GraphQLServiceContext, next: () => Promise<any>) => Promise<void>
 }
 
-export const upload = graphqlUploadKoa({
-  maxFieldSize: 1e6, // size in Bytes
-  maxFileSize: 4 * 1e6,  // size in Bytes
-  maxFiles: 5,
-})
+function graphqlUploadKoaMiddleware(ctx: GraphQLServiceContext, next: () => Promise<any>): Promise<void> {
+  return call(
+    graphqlUploadKoa({
+      maxFieldSize: 1e6, // size in Bytes
+      maxFileSize: 4 * 1e6, // size in Bytes
+      maxFiles: 5,
+    }),
+    ctx,
+    next
+  )
+}
+
+export const upload = graphqlUploadKoaMiddleware
