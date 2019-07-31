05de39be2d9276a23fe33f6afbd111e787bb70a0
Roger Leite Lucena <roger.lucena@polytechnique.edu>
2019-07-31 18:55:35 -0300

    Handle translatable string arrays too

--- src/service/graphql/schema/schemaDirectives/Translatable.ts
+++ src/service/graphql/schema/schemaDirectives/Translatable.ts
@@ -1,8 +1,10 @@
 import { defaultFieldResolver, GraphQLField } from 'graphql'
 import { SchemaDirectiveVisitor } from 'graphql-tools'
+import { map } from 'ramda'
 
 import { ServiceContext } from '../../../typings'
 import { messagesLoader } from '../messagesLoader'
+
 
 export class Translatable extends SchemaDirectiveVisitor {
   public visitFieldDefinition (field: GraphQLField<any, ServiceContext>) {
@@ -17,43 +19,78 @@ export class Translatable extends SchemaDirectiveVisitor {
         }
       }
 
+      const isNotTranslatableString = (response: any) => {
+        return typeof response !== 'string' && typeof response !== 'object'
+      }
+
+      const isNotTranslatableStringArray = (response: any) => {
+        if(!Array.isArray(response)) {
+          return true
+        }
+
+        let isArrayWithOnlyTranslatableStrings = true
+        response.map(
+          element => {
+            if(isNotTranslatableString(element)) {
+              isArrayWithOnlyTranslatableStrings = false // find a way to optmize and break the "map"  process in this case
+            }
+          }
+        )
+
+        return !isArrayWithOnlyTranslatableStrings
+      }
+
       const response = await resolve(root, args, context, info)
 
-      // Messages only knows how to process non empty strings.
-      if ((typeof response !== 'string' && typeof response !== 'object') || Array.isArray(response) || response == null) {
+      // Messages only knows how to process non empty strings, here we also allow array of strings
+      if (isNotTranslatableString(response) || isNotTranslatableStringArray(response)  || response == null) {
         return response
       }
 
-      const resObj = typeof response === 'string'
-        ? {
-          content: response,
-          description: '',
-          from: undefined,
-          id: response,
-        }
-        : response
-      const { content, from, id } = resObj
+      const handleTranslatableString = async (response: any) => {
+        const resObj = typeof response === 'string'
+          ? {
+            content: response,
+            description: '',
+            from: undefined,
+            id: response,
+          }
+          : response
+        const { content, from, id } = resObj
 
       const to =
         locale != null
           ? locale
           : (await segment.getSegment()).cultureInfo
 
-      if (content == null && id == null) {
-        throw new Error(`@translatable directive needs a content or id to translate, but received ${JSON.stringify(response)}`)
+        if (content == null && id == null) {
+          throw new Error(`@translatable directive needs a content or id to translate, but received ${JSON.stringify(response)}`)
+        }
+
+        // If the message is already in the target locale, return the content.
+        if (!to || from === to) {
+          return content
+        }
+
+        return context.loaders.messages!.load({
+          ...resObj,
+          from,
+          to,
+        })
       }
 
-      // If the message is already in the target locale, return the content.
-      if (!to || from === to) {
-        return content
+      const handleTranslatableStringArray = async (response: any) => {
+        return map (
+          (element: any) => handleTranslatableString(element),
+          response
+        )
       }
 
-      return context.loaders.messages!.load({
-        ...resObj,
-        behavior,
-        from,
-        to,
-      })
+      if(Array.isArray(response)) {
+        return handleTranslatableStringArray(response)
+      } else {
+        return handleTranslatableString(response)
+      }
     }
   }
 }
