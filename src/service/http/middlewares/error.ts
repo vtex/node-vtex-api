e77c1e356d68c6c8ca9c4d1936b5371ce4667152
Rebeca Araripe <rebecaararipe@poli.ufrj.br>
2019-08-22 20:16:31 -0300

    Test if ctx.logger and ctx.serverTiming yield error;

--- src/service/http/middlewares/error.ts
+++ src/service/http/middlewares/error.ts
@@ -84,8 +84,9 @@ export async function error<T extends IOClients, U, V> (ctx: ServiceContext<T, U
     }
 
     // Use sendLog directly to avoid cleaning error twice.
-    if (ctx.vtex.logger){
-      ctx.vtex.logger!.log(log, level)
-    }
+    ctx.vtex.logger!.log(log, level)
+    // if (ctx.vtex.logger){
+    //   ctx.vtex.logger!.log(log, level)
+    // }
   }
 }
