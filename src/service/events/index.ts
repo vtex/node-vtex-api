9b8873e0192964f95891b43a39584aaccf290e31
Rebeca Araripe <rebecaararipe@poli.ufrj.br>
2019-08-22 11:07:15 -0300

    Make work locally;

--- src/service/events/index.ts
+++ src/service/events/index.ts
@@ -16,7 +16,8 @@ export const createEventHandler = <ClientsT extends IOClients, StateT, CustomT>(
 ) => {
   return (handler: EventHandler<ClientsT, StateT> | Array<EventHandler<ClientsT, StateT>>) => {
     const middlewares = Array.isArray(handler) ? handler : [handler]
-    const pipeline = [clients(Clients, options), timings, error, contextAdapter<ClientsT, StateT, CustomT>(middlewares)]
+    // const pipeline = [clients(Clients, options), timings, error, contextAdapter<ClientsT, StateT, CustomT>(middlewares)]
+    const pipeline = [clients(Clients, options), timings, error, contextAdapter<ClientsT, StateT, CustomT>(middlewares) ]
     return composeForEvents(pipeline)
   }
 }
