4007e4a2066b9656b05649bf304b9d0b930857e8
Rebeca Araripe <rebecaararipe@poli.ufrj.br>
2019-08-27 20:19:20 -0300

    Remove forgotten if in vtex.logger in error graphql middleware;

--- src/service/graphql/middlewares/error.ts
+++ src/service/graphql/middlewares/error.ts
@@ -118,9 +118,7 @@ export async function graphqlError (ctx: GraphQLServiceContext, next: () => Prom
         if (!level || !(level === LogLevel.Error || level === LogLevel.Warn)) {
           level = LogLevel.Error
         }
-        if (ctx.vtex.logger){
-          ctx.vtex.logger.log(log, level)
-        }
+        ctx.vtex.logger.log(log, level)
       }, uniqueErrors)
 
       // Expose graphQLErrors with pathNames to timings middleware
