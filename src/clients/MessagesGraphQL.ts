f69c80be431922ced6eafdc0635f3dc89a887464
Rebeca Araripe <rebecaararipe@poli.ufrj.br>
2019-08-27 10:56:07 -0300

    Create translate2 method in MessagesGraphQL client;

--- src/clients/MessagesGraphQL.ts
+++ src/clients/MessagesGraphQL.ts
@@ -5,10 +5,18 @@ import { IOContext } from '../service/typings'
 import { IOMessage } from '../utils/message'
 
 type IOMessageInput = Pick<IOMessage, 'id' | 'content' | 'description' | 'behavior'>
+type Behavior = 'USER_ONLY' | 'USER_AND_APP' | 'FULL'
 
 interface MessagesInput {
   provider: string,
   messages: IOMessageInput[],
+}
+
+interface MessageInput {
+  context?: string
+  content: string
+  description?: string
+  behavior?: Behavior
 }
 
 export interface IOMessageSaveInput extends IOMessageInput {
@@ -17,6 +25,12 @@ export interface IOMessageSaveInput extends IOMessageInput {
 
 export interface Translate {
   messages: MessagesInput[]
+  from?: string
+  to: string
+}
+
+export interface Translate2 {
+  messages: MessageInput[]
   from?: string
   to: string
 }
@@ -31,6 +45,10 @@ export interface SaveArgs {
 
 interface TranslateResponse {
   newTranslate: string[]
+}
+
+interface TranslateResponse2 {
+  translate: string[]
 }
 
 export class MessagesGraphQL extends AppGraphQLClient {
@@ -48,6 +66,17 @@ export class MessagesGraphQL extends AppGraphQLClient {
   }, {
     metric: 'messages-translate',
   }).then(path(['data', 'newTranslate'])) as Promise<TranslateResponse['newTranslate']>
+
+  public translate2 = async (args: Translate2): Promise<string[]> => this.graphql.query<TranslateResponse2, { args: Translate2 }>({
+    query: `
+    query Translate($args: TranslateArgs!) {
+      translate(args: $args)
+    }
+    `,
+    variables: { args },
+  }, {
+    metric: 'messages-translate',
+  }).then(path(['data', 'translate'])) as Promise<TranslateResponse2['translate']>
 
   public save = (args: SaveArgs): Promise<boolean> => this.graphql.mutate<boolean, { args: SaveArgs }>({
     mutate: `
