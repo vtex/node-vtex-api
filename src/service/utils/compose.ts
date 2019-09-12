15a213447376b49b25fd5fc69ed1bca9cc4817c1
NataliaGodot <natalia_godot@hotmail.com>
2019-09-12 20:00:25 -0300

    Fix wrong return and improve code

--- src/service/utils/compose.ts
+++ src/service/utils/compose.ts
@@ -1,4 +1,5 @@
 import koaCompose from 'koa-compose'
+import { pipe } from 'ramda'
 
 import { IOClients } from '../../clients/IOClients'
 import { cancel } from '../../utils/cancel'
@@ -6,7 +7,7 @@ import { timer, timerForEvents } from '../../utils/time'
 import { EventHandler, RouteHandler } from '../typings'
 
 export const compose = <ClientsT extends IOClients, StateT, CustomT>(middlewares: Array<RouteHandler<ClientsT, StateT, CustomT>>) =>
-  koaCompose(middlewares.map(timer).map(cancel))
+  koaCompose(middlewares.map(pipe(timer, cancel)))
 
 export const composeForEvents = <ClientsT extends IOClients, StateT>(middlewares: Array<EventHandler<ClientsT, StateT> | any>) =>
   koaCompose(middlewares.map(timerForEvents))
