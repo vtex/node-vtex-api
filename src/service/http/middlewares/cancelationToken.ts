dd5395a6baeac3e04bcc1cbcce4ebe60eb3f429b
NataliaGodot <natalia_godot@hotmail.com>
2019-09-20 16:43:01 -0300

    Use cancellation token for HTTP safe methods

--- src/service/http/middlewares/cancelationToken.ts
+++ src/service/http/middlewares/cancelationToken.ts
@@ -4,7 +4,7 @@ import { IOClients } from '../../../clients/IOClients'
 import { ServiceContext } from '../../typings'
 
 export async function cancelationToken<T extends IOClients, U, V>(ctx: ServiceContext<T, U, V>, next: () => Promise<any>) {
-  if (ctx.method.toUpperCase() === 'GET') {
+  if (ctx.method.toUpperCase() === ('GET' || 'OPTIONS' || 'HEAD')) {
     ctx.vtex.cancellation = {
       cancelable: true,
       cancelled: false,
