3f0af5494521fe4d39304e3723e2e43d3eb3a756
Victor Raposo <vwraposo@gmail.com>
2019-11-05 09:55:54 -0300

    Updates appClient route

--- src/HttpClient/AppClient.ts
+++ src/HttpClient/AppClient.ts
@@ -9,9 +9,17 @@ import { AuthType, InstanceOptions } from './typings'
 export class AppClient extends IOClient {
   constructor(app: string, context: IOContext, options?: InstanceOptions) {
     const {account, workspace, region} = context
-    const [vendor, name] = app.split('.') // vtex.messages
-    const service = [name, vendor].join('.') // messages.vtex
-    const baseURL = `http://${service}.${region}.vtex.io/${account}/${workspace}`
+    const [appName, appVersion] = app.split('@')
+    const [vendor, name] = appName.split('.') // vtex.messages
+    let baseURL: string
+    if (appVersion) {
+      const [major] = appVersion.split('.')
+      baseURL = `http://app.io.vtex.com/${vendor}.${name}/v${major}/${account}/${workspace}`
+    } else {
+      context.logger.warn(`${account} in ${workspace} is using old routing for ${app}`)
+      const service = [name, vendor].join('.') // messages.vtex
+      baseURL = `http://${service}.${region}.vtex.io/${account}/${workspace}`
+    }
 
     super(
       context,
