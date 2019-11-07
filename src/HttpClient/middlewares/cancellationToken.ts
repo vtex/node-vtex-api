92f4b9913c473d12669f9301774f73dd1365ef75
Bruno Abreu <bruno.abreu@vtex.com.br>
2019-11-07 14:44:01 -0300

    Fix cancellation handling algorithm

--- src/HttpClient/middlewares/cancellationToken.ts
+++ src/HttpClient/middlewares/cancellationToken.ts
@@ -1,57 +1,67 @@
+import axios from 'axios'
 import { Cancellation } from '../../service/typings'
 import { MiddlewareContext } from '../typings'
 
 const production = process.env.VTEX_PRODUCTION === 'true'
 
+const handleCancellation = (ctx: MiddlewareContext, cancellation: Cancellation) => {
+  let cancellable = true
+  return {
+    cancelToken: new axios.CancelToken((canceller) => {
+      cancellation.source.token.promise.then(cancel => {
+        if (cancellable) {
+          canceller(cancel.message)
+        }
+      })
+    }),
+    onRequestFinish: () => {
+      if (!ctx.config.cancelToken) {
+        // don't have cancelToken: not cancelable
+        cancellable = false
+      } else if (!ctx.response) {
+        // response is not ready: cancelable
+        cancellable = true
+      } else if (ctx.config.responseType !== 'stream') {
+        // response is ready and it is not a stream: not cancelable
+        cancellable = false
+      } else if (ctx.response.data.readableEnded) {
+        // response stream has ended: not cancelable
+        cancellable = false
+      } else {
+        // when response stream ends: not cancelable
+        ctx.response.data.on('end', function streamEnded() {
+          cancellable = false
+        })
+      }
+    },
+  }
+}
+
 export const cancellationToken = (cancellation?: Cancellation) => async (ctx: MiddlewareContext, next: () => Promise<void>) => {
   const { config: { method } } = ctx
 
   if (!cancellation) {
-    await next()
-    return
-  }
-  if (method && method.toUpperCase() !== 'GET') {
-    cancellation.cancelable = false
-  } else if (cancellation.source) {
-    if (!cancellation.source.token.throwIfRequested && !production) {
-      throw new Error('Missing cancellation function. Are you trying to use HttpClient via workers threads?')
-    } else {
-      ctx.config.cancelToken = cancellation.source.token
-    }
-  }
-
-  await next()
-}
-
-export const cancellationPromise = async (ctx: MiddlewareContext, next: () => Promise<void>) => {
-  const {config: {cancelToken}} = ctx
-  if (!cancelToken) {
     return await next()
   }
 
-  let responsePending = true
-  const cancelPromise = cancelToken.promise
-  cancelToken.promise = new Promise((resolve, reject) => {
-    cancelPromise.then(cancel => {
-      if (responsePending) {
-        resolve(cancel)
-      }
-    }).catch(err => {
-      if (responsePending) {
-        reject(err)
-      }
-    })
-  })
+  if (method && method.toUpperCase() !== 'GET') {
+    cancellation.cancelable = false
+  }
+
+  if (!cancellation.cancelable || !cancellation.source) {
+    return await next()
+  }
+
+  if (!cancellation.source.token.throwIfRequested && !production) {
+    throw new Error('Missing cancellation function. Are you trying to use HttpClient via workers threads?')
+  }
+
+  const {onRequestFinish, cancelToken} = handleCancellation(ctx, cancellation)
+  ctx.config.cancelToken = cancelToken
 
   try {
     await next()
   } finally {
-    if (ctx.response && ctx.config.responseType === 'stream') {
-      ctx.response.data.on('end', function streamEnded() {
-        responsePending = false
-      })
-    } else {
-      responsePending = false
-    }
+    onRequestFinish()
   }
 }
