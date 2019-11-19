779b0563850b6cc899434405e519a780e67d6f0e
Victor Raposo <vwraposo@gmail.com>
2019-11-19 16:16:02 -0300

    Uses https if not from IO

--- src/HttpClient/AppClient.ts
+++ src/HttpClient/AppClient.ts
@@ -3,6 +3,8 @@ import { IOContext } from '../service/typings'
 import { IOClient } from './IOClient'
 import { AuthType, InstanceOptions } from './typings'
 
+const useHttps = !process.env.VTEX_IO
+
 /**
  * Used to perform calls on apps you declared a dependency for in your manifest.
  */
@@ -11,14 +13,15 @@ export class AppClient extends IOClient {
     const {account, workspace, region} = context
     const [appName, appVersion] = app.split('@')
     const [vendor, name] = appName.split('.') // vtex.messages
+    const protocol = useHttps ? 'https' : 'http'
     let baseURL: string
     if (appVersion) {
       const [major] = appVersion.split('.')
-      baseURL = `http://app.io.vtex.com/${vendor}.${name}/v${major}/${account}/${workspace}`
+      baseURL = `${protocol}://app.io.vtex.com/${vendor}.${name}/v${major}/${account}/${workspace}`
     } else {
       context.logger.warn(`${account} in ${workspace} is using old routing for ${app}`)
       const service = [name, vendor].join('.') // messages.vtex
-      baseURL = `http://${service}.${region}.vtex.io/${account}/${workspace}`
+      baseURL = `${protocol}://${service}.${region}.vtex.io/${account}/${workspace}`
     }
 
     super(
--- src/HttpClient/AppGraphQLClient.ts
+++ src/HttpClient/AppGraphQLClient.ts
@@ -2,6 +2,7 @@ import { IOContext } from '../service/typings'
 import { IOGraphQLClient } from './IOGraphQLClient'
 import { AuthType, InstanceOptions } from './typings'
 
+const useHttps = !process.env.VTEX_IO
 /**
  * Used to perform calls on apps you declared a dependency for in your manifest.
  */
@@ -10,14 +11,15 @@ export class AppGraphQLClient extends IOGraphQLClient {
     const {account, workspace, region} = context
     const [appName, appVersion] = app.split('@')
     const [vendor, name] = appName.split('.') // vtex.messages
+    const protocol = useHttps ? 'https' : 'http'
     let baseURL: string
     if (appVersion) {
       const [major] = appVersion.split('.')
-      baseURL = `http://app.io.vtex.com/${vendor}.${name}/v${major}/${account}/${workspace}/_v/graphql`
+      baseURL = `${protocol}://app.io.vtex.com/${vendor}.${name}/v${major}/${account}/${workspace}/_v/graphql`
     } else {
       context.logger.warn(`${account} in ${workspace} is using old routing for ${app}`)
       const service = [name, vendor].join('.') // messages.vtex
-      baseURL = `http://${service}.${region}.vtex.io/${account}/${workspace}/_v/graphql`
+      baseURL = `${protocol}://${service}.${region}.vtex.io/${account}/${workspace}/_v/graphql`
     }
 
     super(
