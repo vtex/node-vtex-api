1f7257033786aacaeb0863d50fee85681dd7fd7a
Victor Raposo <vwraposo@gmail.com>
2019-11-19 16:16:02 -0300

    Uses https if not from IO

--- src/HttpClient/InfraClient.ts
+++ src/HttpClient/InfraClient.ts
@@ -3,6 +3,7 @@ import { IOContext } from '../service/typings'
 import { IOClient } from './IOClient'
 import { AuthType, InstanceOptions } from './typings'
 
+const useHttps = !process.env.VTEX_IO
 /**
  * Used to perform calls on infra apps (e.g. sphinx, apps, vbase).
  */
@@ -10,15 +11,16 @@ export class InfraClient extends IOClient {
   constructor(app: string, context: IOContext, options?: InstanceOptions, isRoot: boolean = false) {
     const {account, workspace, region} = context
     const [appName, appVersion] = app.split('@')
+    const protocol = useHttps ? 'https' : 'http'
     let baseURL: string
     if (appVersion) {
       const [appMajor] = appVersion.split('.')
-      baseURL = `http://infra.io.vtex.com/${appName}/v${appMajor}${isRoot ? '' : `/${account}/${workspace}`}`
+      baseURL = `${protocol}://infra.io.vtex.com/${appName}/v${appMajor}${isRoot ? '' : `/${account}/${workspace}`}`
     } else if (app === 'router') {
-      baseURL = `http://platform.io.vtex.com/${isRoot ? '' : `/${account}/${workspace}`}`
+      baseURL = `${protocol}://platform.io.vtex.com/${isRoot ? '' : `/${account}/${workspace}`}`
     } else {
       context.logger.warn(`${account} in ${workspace} is using old routing for ${app}`)
-      baseURL = `http://${app}.${region}.vtex.io${isRoot ? '' : `/${account}/${workspace}`}`
+      baseURL = `${protocol}://${app}.${region}.vtex.io${isRoot ? '' : `/${account}/${workspace}`}`
     }
 
     super(
