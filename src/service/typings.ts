a6068f564c29794515293ec3847fc8f961203c7c
Tiago Gimenes <tlgimenes@gmail.com>
2019-11-21 15:18:09 -0300

    make workers api more simple

--- src/service/typings.ts
+++ src/service/typings.ts
@@ -149,7 +149,7 @@ export interface ServiceDescriptor {
   },
   minReplicas?: number,
   maxReplicas?: number,
-  workers?: 'automatic' | number
+  workers?: number
 }
 
 export type JanusEnv = string
