0752ed5fca3a31320b8e365324b760dfae7e64f3
Tiago Gimenes <tlgimenes@gmail.com>
2019-11-21 16:49:10 -0300

    adds logOnce capabilities

--- src/service/logger/index.ts
+++ src/service/logger/index.ts
@@ -1,5 +1,8 @@
+import { isMaster, isWorker } from 'cluster'
+
 import { IOContext } from '../../service/typings'
 import { cleanError } from '../../utils/error'
+import { LRUCache } from './../../caches/LRUCache'
 
 const linked = !!process.env.VTEX_APP_LINK
 const app = process.env.VTEX_APP_ID
@@ -12,13 +15,22 @@ export enum LogLevel {
   Error = 'error',
 }
 
+export const log = (message: any, level: LogLevel) => {
+  const logger = console[level]
+  if (typeof logger === 'function') {
+    logger(message)
+  }
+}
+
 export class Logger {
   private account: string
   private workspace: string
   private operationId: string
   private requestId: string
   private production: boolean
-  private executed: boolean = false
+  private history: LRUCache<string, boolean> = new LRUCache({
+    max: 42,
+  })
 
   constructor(ctx: Pick<IOContext, 'account'|'workspace'|'requestId'|'operationId'|'production'>) {
     this.account = ctx.account
@@ -59,17 +71,37 @@ export class Logger {
     this.logSplunkQuery()
   }
 
+  // Since we are now using clusters, if we simply console.log something,
+  // it may overwhelm the programmer's console with lots of repeated info.
+  // This function should be used when you want to warn about something
+  // only once
+  public logOnce = (message: any, level: LogLevel): void => {
+    const strigified = JSON.stringify(message)
+    if (!this.history.has(strigified)) {
+      this.history.set(strigified, true)
+
+      if (isMaster) {
+        log(message, level)
+      }
+      else if (isWorker && process.send) {
+        process.send({
+          message,
+          level,
+          cmd: 'logOnce',
+        })
+      }
+    }
+  }
+
   /**
    * Logs splunk query so the developer can search for the errors in splunk.
    * This function runs only once in the lifetime of the Logger class so we
    * don't mess up with the developer's terminal
    */
   private logSplunkQuery = () => {
-    if (!this.executed && linked) {
-      console.log(
-        `Try this query at Splunk to retrieve error log: 'index=io_vtex_logs app="${app}" account=${this.account} workspace=${this.workspace} level=error OR level=warn'`
-      )
-      this.executed = true
+    if (linked) {
+      const message = `Try this query at Splunk to retrieve error log: 'index=io_vtex_logs app="${app}" account=${this.account} workspace=${this.workspace} level=error OR level=warn'`
+      this.logOnce(message, LogLevel.Info)
     }
   }
 }
