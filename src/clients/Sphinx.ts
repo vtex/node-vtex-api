6dc99f0dceaeedae11771d4cdf2e378cff0cb0e9
pedro823 <grey.pedrinho@gmail.com>
2019-12-03 15:37:07 -0300

    Adds isAdmin function as well

--- src/clients/Sphinx.ts
+++ src/clients/Sphinx.ts
@@ -1,17 +1,21 @@
 import { IOContext } from '../service/typings'
 import { InfraClient, InstanceOptions } from './../HttpClient'
 
-const routes = {
-  ValidatePolicies: '/policies/validate',
-}
-
 export class Sphinx extends InfraClient {
   constructor (ioContext: IOContext, opts?: InstanceOptions) {
     super('sphinx@0.x', ioContext, opts, false)
   }
 
   public validatePolicies = (policies: PolicyRequest[]) => {
-    return this.http.post<void>(routes.ValidatePolicies, policies)
+    return this.http.post<void>('/policies/validate', policies, {
+      metric: 'sphinx-validate-policy',
+    })
+  }
+
+  public isAdmin = (email: string) => {
+    return this.http.get<boolean>(`/user/${email}/isAdmin`, {
+      metric: 'sphinx-is-admin',
+    })
   }
 }
 
