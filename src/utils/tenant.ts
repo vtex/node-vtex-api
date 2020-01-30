2049c80f67e6c8335ba214b3654abe9a4966cacf
Victor Raposo <vwraposo@gmail.com>
2020-01-30 12:16:17 -0300

    Changes header types

--- src/utils/tenant.ts
+++ src/utils/tenant.ts
@@ -1,9 +1,9 @@
-export interface Tenant {
+interface TenantHeader {
   locale: string
 }
 
-export const formatTenantHeaderValue = (tenant: Tenant): string => tenant.locale
+export const formatTenantHeaderValue = (tenant: TenantHeader): string => tenant.locale
 
-export const parseTenantHeaderValue = (value: string): Tenant => ({
+export const parseTenantHeaderValue = (value: string): TenantHeader => ({
   locale: value,
 })
