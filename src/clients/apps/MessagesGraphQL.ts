1b9a7053266908db240875796ae1b1d22d6a4f3d
Tiago Gimenes <tlgimenes@gmail.com>
2020-01-31 12:49:23 -0300

    Support new messages api

--- src/clients/apps/MessagesGraphQL.ts
+++ src/clients/apps/MessagesGraphQL.ts
@@ -2,39 +2,23 @@ import { flatten, path, splitEvery, values } from 'ramda'
 
 import { InstanceOptions } from '../../HttpClient'
 import { IOContext } from '../../service/worker/runtime/typings'
-import { IOMessage } from '../../utils/message'
 import { AppGraphQLClient } from './AppGraphQLClient'
 
-type IOMessageInput = Pick<IOMessage, 'id' | 'content' | 'description' | 'behavior'>
-
-export interface IndexedMessageV2 {
-  messages: IOMessageInputV2[]
+export interface IndexedByFrom {
+  messages: Array<Omit<Message, 'from'>>
   from: string
-}
-
-export interface IOMessageInputV2 {
-  content: string
-  context?: string
-  behavior?: Behavior
 }
 
 export type Behavior = 'FULL' | 'USER_ONLY' | 'USER_AND_APP'
 
-export interface IOMessageV2 extends IOMessageInputV2 {
-  from: string
-  to: string
-}
-
-interface MessagesInput {
-  provider: string,
-  messages: IOMessageInput[],
-}
-
-export interface IOMessageSaveInput extends IOMessageInput {
+export interface Message {
   content: string
+  context?: string
+  behavior?: Behavior
+  from: string
 }
 
-export interface MessageSaveInputV2 {
+export interface MessageSaveInput {
   srcLang: string
   srcMessage: string
   context?: string
@@ -42,34 +26,34 @@ export interface MessageSaveInputV2 {
   groupContext?: string
 }
 
-export interface Translate {
-  messages: MessagesInput[]
-  from?: string
-  to: string
-}
-
-export interface TranslateInputV2 {
-  indexedByFrom: IndexedMessageV2[]
+export interface TranslateInput {
+  indexedByFrom: IndexedByFrom[]
   to: string
   depTree?: string
+  encoding?: 'ICU' | 'HANDLEBARS'
 }
 
-export interface SaveArgs {
+export interface TranslateInput {
+  indexedByFrom: IndexedByFrom[]
   to: string
-  messagesByProvider: Array<{
-    messages: IOMessageSaveInput[]
-    provider: string
-  }>
+  encoding?: 'ICU' | 'HANDLEBARS'
 }
 
-export interface SaveArgsV2 {
+export interface TranslateWithDependenciesInput {
+  indexedByFrom: IndexedByFrom[]
+  to: string
+  depTree: string
+  encoding?: 'ICU' | 'HANDLEBARS'
+}
+
+interface TranslatedWithDependencies {
+  translateWithDependencies: string[]
+}
+
+export interface SaveInput {
   fireEvent?: boolean
   to: string
-  messages: MessageSaveInputV2[]
-}
-
-interface TranslateResponse {
-  newTranslate: string[]
+  messages: MessageSaveInput[]
 }
 
 interface TranslatedV2 {
@@ -83,23 +67,12 @@ export class MessagesGraphQL extends AppGraphQLClient {
     super('vtex.messages@1.x', vtex, options)
   }
 
-  public translate = async (args: Translate): Promise<string[]> => this.graphql.query<TranslateResponse, { args: Translate }>({
-    query: `
-    query Translate($args: NewTranslateArgs!) {
-      newTranslate(args: $args)
-    }
-    `,
-    variables: { args },
-  }, {
-    metric: 'messages-translate',
-  }).then(path(['data', 'newTranslate'])) as Promise<TranslateResponse['newTranslate']>
-
-  public translateV2 = (args: TranslateInputV2) => {
+  public translateV2 (args: TranslateInput) {
     const { indexedByFrom, ...rest } = args
 
-    const allMessages: Array<{from: string, message: IOMessageInputV2}> = indexedByFrom.reduce((acc, {from, messages}) => {
+    const allMessages: Array<{from: string, message: Omit<Message, 'from'>}> = indexedByFrom.reduce((acc, {from, messages}) => {
       return acc.concat(messages.map(message => ({from, message})))
-    }, [] as Array<{from: string, message: IOMessageInputV2}>)
+    }, [] as Array<{from: string, message: Omit<Message, 'from'>}>)
 
     const batchedMessages = splitEvery(MAX_BATCH_SIZE, allMessages)
     return Promise.all(batchedMessages.map(batch => {
@@ -112,12 +85,12 @@ export class MessagesGraphQL extends AppGraphQLClient {
         }
         acc[from].messages.push(message)
         return acc
-      }, {} as Record<string, IndexedMessageV2>)
+      }, {} as Record<string, IndexedByFrom>)
       const batchArgs = {
         ...rest,
         indexedByFrom: values(indexedBatch),
       }
-      return this.graphql.query<TranslatedV2, { args: TranslateInputV2 }>({
+      return this.graphql.query<TranslatedV2, { args: TranslateInput }>({
         query: `
       query Translate($args: TranslateArgs!) {
         translate(args: $args)
@@ -130,27 +103,50 @@ export class MessagesGraphQL extends AppGraphQLClient {
     })).then(flatten)
   }
 
-  public save = (args: SaveArgs): Promise<boolean> => this.graphql.mutate<boolean, { args: SaveArgs }>({
-    mutate: `
-    mutation Save($args: SaveArgs!) {
-      save(args: $args)
-    }
-    `,
-    variables: { args },
-  }, {
-    metric: 'messages-save-translation',
-  }).then(path(['data', 'save'])) as Promise<boolean>
+  public async translate (args: TranslateInput) {
+    const response = await this.graphql.query<TranslatedV2, { args: TranslateInput }>(
+      {
+        query: `query Translate($args: TranslateArgs!) {
+          translate(args: $args)
+        }`,
+        variables: { args },
+      },
+      {
+        metric: 'messages-translate-v2',
+      }
+    )
+    return response.data!.translate
+  }
 
-  public saveV2 = (args: SaveArgsV2): Promise<boolean> => this.graphql.mutate<boolean, { args: SaveArgsV2 }>({
-    mutate: `
-    mutation SaveV2($args: SaveArgsV2!) {
-      saveV2(args: $args)
-    }
-    `,
-    variables: { args },
-  }, {
-    metric: 'messages-saveV2-translation',
-  }).then(path(['data', 'saveV2'])) as Promise<boolean>
+  public async translateWithDependencies (args: TranslateWithDependenciesInput) {
+    const response = await this.graphql.query<TranslatedWithDependencies, { args: TranslateWithDependenciesInput }>(
+      {
+        query: `query TranslateWithDeps($args: TranslateWithDependenciesArgs!) {
+          translateWithDependencies(args: $args)
+        }`,
+        variables: { args },
+      },
+      {
+        metric: 'messages-translate-with-deps-v2',
+      }
+    )
+    return response.data!.translateWithDependencies
+  }
 
+
+  public async saveV2 (args: SaveInput) {
+    const response = await this.graphql.mutate<{ saveV2: boolean }, { args: SaveInput }>(
+      {
+        mutate: `mutation SaveV2($args: SaveArgsV2!) {
+          saveV2(args: $args)
+        }`,
+        variables: { args },
+      },
+      {
+        metric: 'messages-saveV2-translation',
+      }
+    )
+    return response.data!.saveV2
+  }
 }
 
