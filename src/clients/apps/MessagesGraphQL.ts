713fdf7ccc5e3306d70ba2ec67814bbad8f3cae6
Tiago Gimenes <tlgimenes@gmail.com>
2020-02-04 15:16:12 -0300

    remove messages client unused method

--- src/clients/apps/MessagesGraphQL.ts
+++ src/clients/apps/MessagesGraphQL.ts
@@ -61,42 +61,6 @@ export class MessagesGraphQL extends AppGraphQLClient {
     super('vtex.messages@1.x', vtex, options)
   }
 
-  public translateV2 (args: TranslateInput) {
-    const { indexedByFrom, ...rest } = args
-
-    const allMessages: Array<{from: string, message: Omit<Message, 'from'>}> = indexedByFrom.reduce((acc, {from, messages}) => {
-      return acc.concat(messages.map(message => ({from, message})))
-    }, [] as Array<{from: string, message: Omit<Message, 'from'>}>)
-
-    const batchedMessages = splitEvery(MAX_BATCH_SIZE, allMessages)
-    return Promise.all(batchedMessages.map(batch => {
-      const indexedBatch = batch.reduce((acc, {from, message}) => {
-        if (!acc[from]) {
-          acc[from] = {
-            from,
-            messages: [],
-          }
-        }
-        acc[from].messages.push(message)
-        return acc
-      }, {} as Record<string, IndexedByFrom>)
-      const batchArgs = {
-        ...rest,
-        indexedByFrom: values(indexedBatch),
-      }
-      return this.graphql.query<TranslatedV2, { args: TranslateInput }>({
-        query: `
-      query Translate($args: TranslateArgs!) {
-        translate(args: $args)
-      }
-      `,
-        variables: { args: batchArgs },
-      }, {
-        metric: 'messages-translate-v2',
-      }).then(path(['data', 'translate'])) as Promise<TranslatedV2['translate']>
-    })).then(flatten)
-  }
-
   public async translate (args: TranslateInput) {
     const response = await this.graphql.query<TranslatedV2, { args: TranslateInput }>(
       {
