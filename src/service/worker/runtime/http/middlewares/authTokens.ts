47ea71dc35e04eb18cbd2e8b82881c988a068050
NataliaGodot <natalia_godot@hotmail.com>
2020-02-28 18:05:27 -0300

    Import constant instead of redeclaring it

--- src/service/worker/runtime/http/middlewares/authTokens.ts
+++ src/service/worker/runtime/http/middlewares/authTokens.ts
@@ -1,9 +1,9 @@
 import { IOClients } from '../../../../../clients/IOClients'
+import { CREDENTIAL_HEADER } from '../../../../../constants'
 import { ParamsContext, RecorderState, ServiceContext } from '../../typings'
 
 const JANUS_ENV_COOKIE_KEY = 'vtex-commerce-env'
 const VTEX_ID_COOKIE_KEY = 'VtexIdclientAutCookie'
-const VTEX_ID_HEADER_KEY = 'x-vtex-credential'
 
 export async function authTokens <
   T extends IOClients,
@@ -12,7 +12,7 @@ export async function authTokens <
 > (ctx: ServiceContext<T, U, V>, next: () => Promise<void>) {
   const { vtex: { account } } = ctx
 
-  ctx.vtex.adminUserAuthToken = ctx.cookies.get(VTEX_ID_COOKIE_KEY) || ctx.headers[VTEX_ID_HEADER_KEY]
+  ctx.vtex.adminUserAuthToken = ctx.cookies.get(VTEX_ID_COOKIE_KEY) || ctx.headers[CREDENTIAL_HEADER]
   ctx.vtex.storeUserAuthToken = ctx.cookies.get(`${VTEX_ID_COOKIE_KEY}_${account}`)
   ctx.vtex.janusEnv = ctx.cookies.get(JANUS_ENV_COOKIE_KEY)
 
