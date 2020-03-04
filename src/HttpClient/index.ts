17349c660d5e855490f85bc83509d9757a597e4e
Tiago Gimenes <tlgimenes@gmail.com>
2020-03-04 12:23:16 -0300

    add agent helpers

--- src/HttpClient/index.ts
+++ src/HttpClient/index.ts
@@ -1,6 +1,7 @@
 export * from './HttpClient'
 export * from './typings'
 export * from './GraphQLClient'
+export * from './agents'
 
 export { Cached, CacheType } from './middlewares/cache'
 export { inflightURL, inflightUrlWithQuery } from './middlewares/inflight'
