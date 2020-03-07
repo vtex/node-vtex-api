851d6a3dbcb393a7d93d2a10ccf75fdf7f311efe
tiagonapoli <tiago.napoli@vtex.com.br>
2020-03-07 20:50:20 -0300

    [tracing-commons] Create UserLandTracer class

---
+++ src/tracing/UserLandTracer.ts
@@ -0,0 +1,46 @@
+import { Span, SpanContext, SpanOptions, Tracer } from 'opentracing'
+
+export interface IUserLandTracer {
+  startSpan: Tracer['startSpan']
+  inject: Tracer['inject']
+}
+
+export class UserLandTracer implements IUserLandTracer {
+  // tslint:disable-next-line
+  #tracer: Tracer
+  // tslint:disable-next-line
+  #fallbackSpan: Span
+  // tslint:disable-next-line
+  #fallbackSpanLock: boolean
+
+  constructor(tracer: Tracer, fallbackSpan: Span) {
+    this.#tracer = tracer
+    this.#fallbackSpan = fallbackSpan
+    this.#fallbackSpanLock = false
+    
+    this.inject = this.#tracer.inject.bind(this.#tracer)
+  }
+
+  public lockFallbackSpan() {
+    this.#fallbackSpanLock = true
+  }
+
+  public setFallbackSpan(newSpan: Span) {
+    if(this.#fallbackSpanLock) {
+      throw new Error(`FallbackSpan is locked, can't change it`)
+    }
+
+    this.#fallbackSpan = newSpan
+  }
+
+  public startSpan(name: string, options: SpanOptions = {}) {
+    if(!options.childOf) {
+        return this.#tracer.startSpan(name, { ...options, childOf: this.#fallbackSpan })
+    }
+    return this.#tracer.startSpan(name, options)
+  }
+
+  public inject(spanContext: SpanContext | Span, format: string, carrier: any) {
+    return this.#tracer.inject(spanContext, format, carrier)
+  }
+}
