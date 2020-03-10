9b6e8c678bec157d3cf9b5bc7bdb4a36adbe5e4f
tiagonapoli <tiago.napoli@vtex.com.br>
2020-03-10 20:42:32 -0300

    [service:tracing:userLandTracer] Do not use hard privacy yet. typescript-json-schema does not support it

--- src/tracing/UserLandTracer.ts
+++ src/tracing/UserLandTracer.ts
@@ -6,41 +6,38 @@ export interface IUserLandTracer {
 }
 
 export class UserLandTracer implements IUserLandTracer {
-  // tslint:disable-next-line
-  #tracer: Tracer
-  // tslint:disable-next-line
-  #fallbackSpan: Span
-  // tslint:disable-next-line
-  #fallbackSpanLock: boolean
+  private tracer: Tracer
+  private fallbackSpan: Span
+  private fallbackSpanLock: boolean
 
   constructor(tracer: Tracer, fallbackSpan: Span) {
-    this.#tracer = tracer
-    this.#fallbackSpan = fallbackSpan
-    this.#fallbackSpanLock = false
+    this.tracer = tracer
+    this.fallbackSpan = fallbackSpan
+    this.fallbackSpanLock = false
     
-    this.inject = this.#tracer.inject.bind(this.#tracer)
+    this.inject = this.tracer.inject.bind(this.tracer)
   }
 
   public lockFallbackSpan() {
-    this.#fallbackSpanLock = true
+    this.fallbackSpanLock = true
   }
 
   public setFallbackSpan(newSpan: Span) {
-    if(this.#fallbackSpanLock) {
+    if(this.fallbackSpanLock) {
       throw new Error(`FallbackSpan is locked, can't change it`)
     }
 
-    this.#fallbackSpan = newSpan
+    this.fallbackSpan = newSpan
   }
 
   public startSpan(name: string, options: SpanOptions = {}) {
     if(!options.childOf) {
-        return this.#tracer.startSpan(name, { ...options, childOf: this.#fallbackSpan })
+        return this.tracer.startSpan(name, { ...options, childOf: this.fallbackSpan })
     }
-    return this.#tracer.startSpan(name, options)
+    return this.tracer.startSpan(name, options)
   }
 
   public inject(spanContext: SpanContext | Span, format: string, carrier: any) {
-    return this.#tracer.inject(spanContext, format, carrier)
+    return this.tracer.inject(spanContext, format, carrier)
   }
 }
