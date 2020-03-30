f3b29cbe138fb9bf28b12ebba88bc8e370dd18be
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-03-30 18:14:53 -0300

    [tracing:ErrorReport] Fix truncateStringsFromObject - StackOverflow error
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/errorReporting/utils.ts
+++ src/tracing/errorReporting/utils.ts
@@ -1,15 +1,33 @@
-export function truncateStringsFromObject(element: any, maxStrSize: number) {
+export function truncateStringsFromObject(element: any, maxStrSize: number, depth = 3, seenObjects: any[] = []) {
   if (element == null) {
     return element
   }
-  if (typeof element === 'object') {
+
+  const objType = typeof element
+
+  if (objType === 'object') {
+    if (seenObjects.includes(element)) {
+      return '[circular]'
+    }
+
+    if (depth === 0) {
+      return Array.isArray(element) ? `[array]` : `[object]`
+    }
+
+    seenObjects.push(element)
     Object.keys(element).forEach(key => {
-      element[key] = truncateStringsFromObject(element[key], maxStrSize)
+      // seenObjects.slice creates a copy of seenObjects at the current state
+      element[key] = truncateStringsFromObject(element[key], maxStrSize, depth - 1, seenObjects.slice())
     })
+
     return element
+  } else if (objType === 'string') {
+    return element.length <= maxStrSize ? element : `${element.substr(0, maxStrSize)}[...TRUNCATED]`
+  } else if (objType === 'number' || objType === 'bigint' || objType === 'boolean' || objType === 'symbol') {
+    return element
+  } else if (objType === 'function') {
+    return `[function: ${element.name || 'anonymous'}]`
   }
-  if (typeof element === 'string' && element.length > maxStrSize) {
-    return `${element.substr(0, maxStrSize)}[...TRUNCATED]`
-  }
-  return element
+
+  return `[${objType}]`
 }
