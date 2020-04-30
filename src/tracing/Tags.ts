b36fcc41905480e0cc81bb7efecf8319f718c500
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-04-30 17:40:50 -0300

    [tracing:tags] Use snake case
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/Tags.ts
+++ src/tracing/Tags.ts
@@ -2,14 +2,14 @@ import { Tags as opentracingTags } from 'opentracing'
 
 const VTEX_INCOMING_REQUEST_TAGS = {
   VTEX_ACCOUNT: 'vtex.incoming.account',
-  VTEX_REQUEST_ID: 'vtex.request-id',
+  VTEX_REQUEST_ID: 'vtex.request_id',
   VTEX_WORKSPACE: 'vtex.incoming.workspace',
 }
 
 const APP_TAGS = {
   VTEX_APP_LINKED: 'app.linked',
-  VTEX_APP_NODE_ENV: 'app.node-env',
-  VTEX_APP_NODE_VTEX_API_VERSION: 'app.node-vtex-api-version',
+  VTEX_APP_NODE_ENV: 'app.node_env',
+  VTEX_APP_NODE_VTEX_API_VERSION: 'app.node_vtex_api_version',
   VTEX_APP_PRODUCTION: 'app.production',
   VTEX_APP_REGION: 'app.region',
   VTEX_APP_VERSION: 'app.version',
@@ -19,7 +19,7 @@ const APP_TAGS = {
 const ERROR_REPORT_TAGS = {
   ERROR_KIND: 'error.kind',
   ERROR_SERVER_CODE: 'error.server.code',
-  ERROR_SERVER_REQUEST_ID: 'error.server.request-id',
+  ERROR_SERVER_REQUEST_ID: 'error.server.request_id',
   ERROR_SERVER_SOURCE: 'error.server.source',
 }
 
@@ -29,10 +29,10 @@ export const USERLAND_TAGS = {
 }
 
 export const Tags = {
-  HTTP_NO_RESPONSE: 'http.no-response',
+  HTTP_NO_RESPONSE: 'http.no_response',
   HTTP_PATH: 'http.path',
-  HTTP_RETRY_COUNT: 'http.retry-count',
-  HTTP_ROUTER_CACHE: 'http.router-cache',
+  HTTP_RETRY_COUNT: 'http.retry_count',
+  HTTP_ROUTER_CACHE: 'http.router_cache',
   ...USERLAND_TAGS,
   ...VTEX_INCOMING_REQUEST_TAGS,
   ...APP_TAGS,
