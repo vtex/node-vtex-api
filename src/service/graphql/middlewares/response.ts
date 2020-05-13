8ed92c93854590508a6204bd5cafa99ed60e9e4d
Bruno Abreu <bruno.abreu@vtex.com.br>
2020-05-13 16:45:16 -0300

    Set vary session for scope private

--- src/service/graphql/middlewares/response.ts
+++ src/service/graphql/middlewares/response.ts
@@ -1,11 +1,27 @@
 import { isEmpty, pick, reject } from 'ramda'
 
-import { FORWARDED_HOST_HEADER, SEGMENT_HEADER } from '../../../constants'
+import { FORWARDED_HOST_HEADER, SEGMENT_HEADER, SESSION_HEADER } from '../../../constants'
 import { GraphQLServiceContext } from '../typings'
 import { cacheControl } from '../utils/cacheControl'
 
 const DEV_FIELDS = ['data', 'errors', 'extensions']
 const PROD_FIELDS = ['data', 'errors']
+
+function setVaryHeaders (ctx: GraphQLServiceContext, scope: string, segment: boolean | null) {
+  ctx.vary(FORWARDED_HOST_HEADER)
+  if (segment) {
+    ctx.vary(SEGMENT_HEADER)
+  }
+
+  if (scope === 'private' || ctx.query.scope === 'private') {
+    ctx.vary(SESSION_HEADER)
+  } else if (ctx.vtex.sessionToken) {
+    ctx.vtex.logger.warn({
+      message: 'GraphQL resolver receiving session token without private scope',
+      userAgent: ctx.headers['user-agent'],
+    })
+  }
+}
 
 export async function response (ctx: GraphQLServiceContext, next: () => Promise<void>) {
   const {responseInit, graphqlResponse} = ctx.graphql
@@ -23,9 +39,8 @@ export async function response (ctx: GraphQLServiceContext, next: () => Promise<
     'Cache-Control': cacheControlHeader,
   })
 
-  ctx.vary(FORWARDED_HOST_HEADER)
-  if (segment) {
-    ctx.vary(SEGMENT_HEADER)
+  if (ctx.method.toUpperCase() === 'GET') {
+    setVaryHeaders(ctx, scope, segment)
   }
 
   const fields = production ? PROD_FIELDS : DEV_FIELDS
