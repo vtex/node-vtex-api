1c596c1fc93b9528b90fbcaa6974af62197a0b25
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-05-15 21:12:56 -0300

    [ErrorReport] Use log fields instead of tags
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/errorReporting/ErrorReport.ts
+++ src/tracing/errorReporting/ErrorReport.ts
@@ -8,6 +8,7 @@ import {
 import { Span } from 'opentracing'
 import { TracingTags } from '..'
 import { IOContext } from '../../service/worker/runtime/typings'
+import { LOG_FIELDS } from '../LogFields'
 import { getTraceInfo } from '../utils'
 
 export class ErrorReport extends ErrorReportBase {
@@ -17,22 +18,26 @@ export class ErrorReport extends ErrorReportBase {
 
   public injectOnSpan(span: Span, logger?: IOContext['logger']) {
     span.setTag(TracingTags.ERROR, 'true')
-    span.setTag(TracingTags.ERROR_KIND, this.kind)
+
+    const indexedLogs: Record<string, string> = {
+      [LOG_FIELDS.ERROR_KIND]: this.kind,
+      [LOG_FIELDS.ERROR_ID]: this.metadata.errorId,
+    }
 
     if (
       isRequestInfo(this.parsedInfo) &&
       this.parsedInfo.response &&
       isInfraErrorData(this.parsedInfo.response?.data)
     ) {
-      span.setTag(TracingTags.ERROR_SERVER_CODE, this.parsedInfo.response.data.code)
-      span.setTag(TracingTags.ERROR_SERVER_SOURCE, this.parsedInfo.response.data.source)
-      span.setTag(TracingTags.ERROR_SERVER_REQUEST_ID, this.parsedInfo.response.data.requestId)
+      indexedLogs[LOG_FIELDS.ERROR_SERVER_CODE] = this.parsedInfo.response.data.code
+      indexedLogs[LOG_FIELDS.ERROR_SERVER_REQUEST_ID] = this.parsedInfo.response.data.requestId
     }
 
-    span.log({ event: 'error', ...this.toObject() })
+    const serializableError = this.toObject()
+    span.log({ [LOG_FIELDS.EVENT]: 'error', ...indexedLogs, error: serializableError })
 
     if (logger && this.shouldLogToSplunk(span)) {
-      logger.error(this.toObject())
+      logger.error(serializableError)
       this.markErrorAsReported()
     }
 
