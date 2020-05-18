4c487717a2ed0ad8c0eb7a27643ca1667baa1912
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-05-18 18:44:56 -0300

    Fixes
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/LogFields.ts
+++ src/tracing/LogFields.ts
@@ -3,15 +3,13 @@
 // When using span.log({ field1: string1, field2: object2, ... })
 // The jaeger backend indexes for search the first-level fields that hold strings
 // These fields then can be searched on jaeger-ui using: 'field1=string'
-// This file concentrates all log fieds used in node-vtex-api that hold
+// This file concentrates all log fields used in node-vtex-api that hold
 // strings and can be searched in the jaeger-ui
 
 const ERROR_REPORT = {
   // ErrorReport class has some meta info on the error
-  // The following log fieds hold this meta info
+  // The following log fields hold this meta info
   ERROR_KIND: 'error.kind',
-  // tslint:disable-
-
   ERROR_ID: 'error.id',
 
   // VTEX's Infra errors adds error details to the response
