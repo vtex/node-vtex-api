2e27fd09d5d62cb1c54d52e9e170ac3119673209
Sávio Muniz <savio.muniz@vtex.com.br>
2020-05-19 17:07:10 -0300

    remove arrow functions and add tracingConfig

--- src/clients/external/Masterdata.ts
+++ src/clients/external/Masterdata.ts
@@ -1,5 +1,5 @@
 import FormData from 'form-data'
-import { InstanceOptions, IOContext, UserInputError } from '../..'
+import { InstanceOptions, IOContext, UserInputError, RequestTracingConfig } from '../..'
 import { ExternalClient } from './ExternalClient'
 
 export class MasterData extends ExternalClient {
@@ -27,118 +27,216 @@ export class MasterData extends ExternalClient {
     }
   }
 
-  public getSchema = <T>(dataEntity: string, schema: string) =>
-    this.http.get<T>(this.routes.schema(dataEntity, schema), {
-      metric: 'masterdata-getSchema',
-    })
+  public getSchema<T>(dataEntity: string, schema: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-getSchema'
 
-  public createOrUpdateSchema = <T>(dataEntity: string, schemaName: string, schemaBody: object ) =>
-    this.http.put<T>(this.routes.schema(dataEntity, schemaName), schemaBody, {
-      metric: 'masterdata-getSchema',
+    return this.http.get<T>(this.routes.schema(dataEntity, schema), {
+      metric,
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public getPublicSchema = <T>(dataEntity: string, schema: string) =>
-    this.http.get<T>(this.routes.publicSchema(dataEntity, schema), {
-      metric: 'masterdata-getPublicSchema',
+  public createOrUpdateSchema<T>(dataEntity: string, schemaName: string, schemaBody: object, tracingConfig?: RequestTracingConfig ){
+    const metric = 'masterdata-getSchema'
+    return this.http.put<T>(this.routes.schema(dataEntity, schemaName), schemaBody, {
+      metric,
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public getDocument = <T>(dataEntity: string, id: string, fields: string[]) =>
-    this.http.get<T>(this.routes.document(dataEntity, id), {
-      metric: 'masterdata-getDocument',
+
+  public getPublicSchema<T>(dataEntity: string, schema: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-getPublicSchema'
+
+    return this.http.get<T>(this.routes.publicSchema(dataEntity, schema), {
+      metric,
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
+    })
+  }
+
+
+  public getDocument<T>(dataEntity: string, id: string, fields: string[], tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-getDocument'
+
+    return this.http.get<T>(this.routes.document(dataEntity, id), {
+      metric,
       params: {
         _fields: generateFieldsArg(fields),
       },
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public createDocument = (dataEntity: string, fields: object, schema?: string) =>
-    this.http.post<DocumentResponse>(this.routes.documents(dataEntity), fields, {
-      metric: 'masterdata-createDocument',
+
+  public createDocument (dataEntity: string, fields: object, schema?: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-createDocument'
+
+    return this.http.post<DocumentResponse>(this.routes.documents(dataEntity), fields, {
+      metric,
       params: {
         ...(schema ? { _schema: schema } : null),
       },
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public createOrUpdateEntireDocument = (dataEntity: string, fields: object, id?: string, schema?: string) =>
-    this.http.put<DocumentResponse>(
+
+  public createOrUpdateEntireDocument (dataEntity: string, fields: object, id?: string, schema?: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-createOrUpdateEntireDocument'
+
+    return this.http.put<DocumentResponse>(
       this.routes.documents(dataEntity),
       { id, ...fields },
       {
-        metric: 'masterdata-createOrUpdateEntireDocument',
+        metric,
         params: {
           ...(schema ? { _schema: schema } : null),
         },
+        tracing: {
+          requestSpanNameSuffix: metric,
+          ...tracingConfig?.tracing,
+        },
       }
     )
+  }
 
-  public createOrUpdatePartialDocument = (dataEntity: string, fields: object, id?: string, schema?: string) =>
-    this.http.patch<DocumentResponse>(
+
+  public createOrUpdatePartialDocument (dataEntity: string, fields: object, id?: string, schema?: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-createOrUpdatePartialDocument'
+
+    return this.http.patch<DocumentResponse>(
       this.routes.documents(dataEntity),
       { id, ...fields },
       {
-        metric: 'masterdata-createOrUpdatePartialDocument',
+        metric,
         params: {
           ...(schema ? { _schema: schema } : null),
         },
+        tracing: {
+          requestSpanNameSuffix: metric,
+          ...tracingConfig?.tracing,
+        },
       }
     )
+  }
 
-  public updateEntireDocument = (dataEntity: string, id: string, fields: object, schema?: string) =>
-    this.http.put(this.routes.document(dataEntity, id), fields, {
-      metric: 'masterdata-updateEntireDocument',
+
+  public updateEntireDocument (dataEntity: string, id: string, fields: object, schema?: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-updateEntireDocument'
+
+    return this.http.put(this.routes.document(dataEntity, id), fields, {
+      metric,
       params: {
         ...(schema ? { _schema: schema } : null),
       },
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public updatePartialDocument = (dataEntity: string, id: string, fields: object, schema?: string) =>
-    this.http.patch(this.routes.document(dataEntity, id), fields, {
-      metric: 'masterdata-updatePartialDocument',
+
+  public updatePartialDocument (dataEntity: string, id: string, fields: object, schema?: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-updatePartialDocument'
+
+    return this.http.patch(this.routes.document(dataEntity, id), fields, {
+      metric,
       params: {
         ...(schema ? { _schema: schema } : null),
       },
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public searchDocuments = <T>(
+  public searchDocuments<T>(
     dataEntity: string,
     fields: string[],
     where: string,
     pagination: PaginationArgs,
     schema?: string,
-    sort?: string
-  ) =>
-    this.http.get<T[]>(this.routes.search(dataEntity), {
+    sort?: string, tracingConfig?: RequestTracingConfig
+  ){
+    const metric = 'masterdata-searchDocuments'
+
+    return this.http.get<T[]>(this.routes.search(dataEntity), {
       headers: paginationArgsToHeaders(pagination),
-      metric: 'masterdata-searchDocuments',
+      metric,
       params: {
         _fields: generateFieldsArg(fields),
         _sort: sort,
         _where: where,
         ...(schema ? { _schema: schema } : null),
       },
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public scrollDocuments = <T>(
+  public scrollDocuments<T>(
     dataEntity: string,
     fields: string[],
-    pagination: PaginationArgs
-  ) => this.http.get(this.routes.scroll(dataEntity), {
-    headers: paginationArgsToHeaders(pagination),
-    metric: 'masterdata-scrollDocuments',
-    params: {
-      _fields: generateFieldsArg(fields),
-    },
-  })
+    pagination: PaginationArgs, tracingConfig?: RequestTracingConfig
+  ){
+    const metric = 'masterdata-scrollDocuments'
 
-  public deleteDocument = (dataEntity: string, id: string) =>
-    this.http.delete(this.routes.document(dataEntity, id), {
-      metric: 'masterdata-deleteDocument',
+    return this.http.get<T[]>(this.routes.scroll(dataEntity), {
+      headers: paginationArgsToHeaders(pagination),
+      metric,
+      params: {
+        _fields: generateFieldsArg(fields),
+      },
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 
-  public uploadAttachment = (dataEntity: string, id: string, fields: string, formData: FormData) =>
-    this.http.post<any>(this.routes.attachments(dataEntity, id, fields), formData, {
+  public deleteDocument(dataEntity: string, id: string, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-deleteDocument'
+
+    return this.http.delete(this.routes.document(dataEntity, id), {
+      metric,
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
+    })
+  }
+
+  public uploadAttachment(dataEntity: string, id: string, fields: string, formData: FormData, tracingConfig?: RequestTracingConfig){
+    const metric = 'masterdata-uploadAttachment'
+
+    return this.http.post<any>(this.routes.attachments(dataEntity, id, fields), formData, {
       headers: formData.getHeaders(),
-      metric: 'masterdata-uploadAttachment',
+      metric,
+      tracing: {
+        requestSpanNameSuffix: metric,
+        ...tracingConfig?.tracing,
+      },
     })
+  }
 }
 
 function paginationArgsToHeaders({ page, pageSize }: PaginationArgs) {
