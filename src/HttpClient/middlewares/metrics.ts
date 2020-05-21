fb0742d88daa8539ba1c9b5c0519710fc592f530
Jeymisson Oliveira <jeymissoncz@gmail.com>
2020-05-21 16:42:01 -0300

    Remove serverTiming headers

--- src/HttpClient/middlewares/metrics.ts
+++ src/HttpClient/middlewares/metrics.ts
@@ -8,31 +8,11 @@ import {
 import {
   formatTimingName,
   hrToMillis,
-  parseTimingName,
   shrinkTimings,
 } from '../../utils'
 import { TIMEOUT_CODE } from '../../utils/retry'
 import { statusLabel } from '../../utils/status'
 import { MiddlewareContext } from '../typings'
-
-const parseServerTiming = (serverTimingsHeaderValue: string) => compose<string, string, string[], Array<[string, string]>>(
-  reduce((acc, rawHeader) => {
-    const [name, durStr] = rawHeader.split(';')
-    const [_, dur] = durStr ? durStr.split('=') : [null, null]
-    const {hopNumber, source, target} = parseTimingName(name)
-    const formatted = formatTimingName({
-      hopNumber: Number.isNaN(hopNumber as any) ? null : hopNumber! + 1,
-      source,
-      target,
-    })
-    if (dur && formatted) {
-      acc.push([formatted, dur])
-    }
-    return acc
-  }, [] as Array<[string, string]>),
-  split(','),
-  replace(/\s/g, '')
-)(serverTimingsHeaderValue)
 
 interface MetricsOpts {
   metrics?: MetricsAccumulator
@@ -147,20 +127,6 @@ export const metricsMiddleware = ({metrics, serverTiming, name}: MetricsOpts) =>
         const dur = hrToMillis(process.hrtime(serverTimingStart))
         if (!serverTiming[serverTimingLabel] || Number(serverTiming[serverTimingLabel]) < dur) {
           serverTiming[serverTimingLabel] = `${dur}`
-        }
-
-        // Forward server timings
-        const serverTimingsHeader = path<string>(['response', 'headers', 'server-timing'], ctx)
-        if (!ctx.cacheHit && !ctx.inflightHit && !ctx.memoizedHit && serverTimingsHeader) {
-          const parsedServerTiming = parseServerTiming(serverTimingsHeader)
-          forEach(
-            ([timingsName, timingsDur]) => {
-              if (!serverTiming[timingsName] || Number(serverTiming[timingsName]) < Number(timingsDur)) {
-                serverTiming[timingsName] = timingsDur
-              }
-            },
-            parsedServerTiming
-          )
         }
       }
     }
