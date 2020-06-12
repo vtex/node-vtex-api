342d1f98a23008d9f79686643a658b92f3f93e59
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-06-12 20:41:08 -0300

    [tracing] Create LOG_EVENTS constant
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/LogFields.ts
+++ src/tracing/LogFields.ts
@@ -1,19 +1,25 @@
 /* tslint:disable:object-literal-sort-keys */
 
-// When using span.log({ field1: string1, field2: object2, ... })
-// The jaeger backend indexes for search the first-level fields that hold strings
-// These fields then can be searched on jaeger-ui using: 'field1=string'
-// This file concentrates all log fields used in node-vtex-api that hold
-// strings and can be searched in the jaeger-ui
+/**
+ * When using span.log({ field1: string1, field2: object2, ... })
+ * The jaeger backend indexes for search the first-level fields that hold strings
+ * These fields then can be searched on jaeger-ui using: 'field1=string'
+ * This file concentrates all log fields used in node-vtex-api that hold
+ * strings and can be searched in the jaeger-ui
+ */
 
 const ERROR_REPORT = {
-  // ErrorReport class has some meta info on the error
-  // The following log fields hold this meta info
+  /**
+   * ErrorReport class has some meta info on the error
+   * The following log fields hold this meta info
+   */
   ERROR_KIND: 'error.kind',
   ERROR_ID: 'error.id',
 
-  // VTEX's Infra errors adds error details to the response
-  // The following log fields are supposed to hold these fields
+  /**
+   * VTEX's Infra errors adds error details to the response
+   * The following log fields are supposed to hold these fields
+   */
   ERROR_SERVER_CODE: 'error.server.code',
   ERROR_SERVER_REQUEST_ID: 'error.server.request_id',
 }
@@ -21,4 +27,12 @@ const ERROR_REPORT = {
 export const LOG_FIELDS = {
   EVENT: 'event',
   ...ERROR_REPORT,
+}
+
+export const LOG_EVENTS = {
+  /** Event representing that userland middlewares are about to start */
+  USER_MIDDLEWARES_START: 'user-middlewares-start',
+
+  /** Event representing that userland middlewares just finished */
+  USER_MIDDLEWARES_FINISH: 'user-middlewares-finish',
 }
