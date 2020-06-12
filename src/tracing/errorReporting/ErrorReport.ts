eabc5c8ce70daee4b1318fb00253901ff0d1d746
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-06-12 16:57:44 -0300

    [fix] Do not recursively wrap ErrorReports
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/errorReporting/ErrorReport.ts
+++ src/tracing/errorReporting/ErrorReport.ts
@@ -12,10 +12,36 @@ import { LOG_FIELDS } from '../LogFields'
 import { getTraceInfo } from '../utils'
 
 export class ErrorReport extends ErrorReportBase {
-  public static create(args: ErrorReportCreateArgs) {
+  /**
+   * Create a new ErrorReport wrapping the args.originalError
+   *
+   * In case the args.originalError argument is a ErrorReport already it just return it
+   * If it's not, it returns a new ErrorReport wrapping the error.
+   * This way you can use this function as a catchAll, e.g.:
+   *
+   * ```
+   * try {
+   *   await next()
+   * } catch(err) {
+   *   ErrorReport.create({ originalError: err }).injectOnSpan(span)
+   * }
+   * ```
+   *
+   * More docs on ErrorReport available on: https://github.com/vtex/node-error-report
+   */
+  public static create(args: ErrorReportCreateArgs): ErrorReport {
+    if (args.originalError instanceof ErrorReport) {
+      return args.originalError
+    }
+
     return new ErrorReport(createErrorReportBaseArgs(args))
   }
 
+  /**
+   * Inject information about the error on the provided Span.
+   * If a logger is provided and the span is part of a **sampled**
+   * trace, then the error will be logged.
+   */
   public injectOnSpan(span: Span, logger?: IOContext['logger']) {
     span.setTag(TracingTags.ERROR, 'true')
 
