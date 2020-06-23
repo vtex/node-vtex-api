c6bf0c9c9c0d10e3667a732df010109b3f0e624f
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-06-23 12:25:34 -0300

    Use new annotations format
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/index.ts
+++ src/tracing/index.ts
@@ -4,7 +4,7 @@ export { ErrorKindsBase as ErrorKinds } from '@vtex/node-error-report'
 export { ErrorReport } from './errorReporting/ErrorReport'
 export { createSpanReference } from './spanReference/createSpanReference'
 export { SpanReferenceTypes } from './spanReference/SpanReferenceTypes'
-export { USERLAND_TAGS as TracingTags } from './Tags'
+export { UserlandTags as TracingTags } from './Tags'
 export { createTracingContextFromCarrier, IUserLandTracer } from './UserLandTracer'
 export { getTraceInfo, TraceInfo } from './utils'
 export { Span, FORMAT_HTTP_HEADERS }
