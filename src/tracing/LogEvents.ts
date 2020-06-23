252195c97302fd7597d9b88d234a46c6886ed5e6
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-06-23 12:17:07 -0300

    [tracing:annotations] Use const enum, refactor and create HTTP cache annotations
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

---
+++ src/tracing/LogEvents.ts
@@ -0,0 +1,43 @@
+/* tslint:disable:object-literal-sort-keys */
+
+/**
+ * This file has all events logged by the runtime's distributed tracing
+ * instrumentation. In the code you would see something like:
+ * ```
+ * span.log({ event: HttpEvents.CACHE_KEY_CREATE })
+ * ```
+ */
+
+export const enum HttpLogEvents {
+  /** Event holding the cache key created */
+  CACHE_KEY_CREATE = 'cache-key-created',
+
+  /** Event representing that the memoization cache has just saved a response */
+  MEMOIZATION_CACHE_SAVED = 'memoization-cache-saved',
+
+  /** Event representing that the memoization cache has just saved a error response */
+  MEMOIZATION_CACHE_SAVED_ERROR = 'memoization-cache-saved-error',
+
+  /** Event holding information on a local cache hit that just happened */
+  LOCAL_CACHE_HIT_INFO = 'local-cache-hit-info',
+
+  /** Event holding information on the cache config that will be used to decide whether or not to update the local cache */
+  CACHE_CONFIG = 'cache-config',
+
+  /** Event informing the decision to not update or save to local cache */
+  NO_LOCAL_CACHE_SAVE = 'no-local-cache-save',
+
+  /** Event holding information on the local cache save that just happened */
+  LOCAL_CACHE_SAVED = 'local-cache-saved',
+
+  /** A request that is retryable just failed - this event will hold info on the retry that may happen */
+  SETUP_REQUEST_RETRY = 'setup-request-retry',
+}
+
+export const enum RuntimeLogEvents {
+  /** Event representing that userland middlewares are about to start */
+  USER_MIDDLEWARES_START = 'user-middlewares-start',
+
+  /** Event representing that userland middlewares just finished */
+  USER_MIDDLEWARES_FINISH = 'user-middlewares-finish',
+}
