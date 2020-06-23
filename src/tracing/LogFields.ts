8dd0449750b39ca780147d0ded13a4bd963790c4
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-06-23 12:17:07 -0300

    [tracing:annotations] Use const enum, refactor and create HTTP cache annotations
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/tracing/LogFields.ts
+++ src/tracing/LogFields.ts
@@ -8,34 +8,69 @@
  * strings and can be searched in the jaeger-ui
  */
 
-const ERROR_REPORT = {
+export const enum ErrorReportLogFields {
   /**
    * ErrorReport class has some meta info on the error
    * The following log fields hold this meta info
    */
-  ERROR_KIND: 'error.kind',
-  ERROR_ID: 'error.id',
+  ERROR_KIND = 'error.kind',
+  ERROR_ID = 'error.id',
 
   /**
    * VTEX's Infra errors adds error details to the response
    * The following log fields are supposed to hold these fields
    */
-  ERROR_SERVER_CODE: 'error.server.code',
-  ERROR_SERVER_REQUEST_ID: 'error.server.request_id',
+  ERROR_SERVER_CODE = 'error.server.code',
+  ERROR_SERVER_REQUEST_ID = 'error.server.request_id',
 }
 
-export const LOG_FIELDS = {
-  EVENT: 'event',
-  
+export const enum RuntimeLogFields {
   /** Time in ms spent to run the user middlewares */
-  USER_MIDDLEWARES_DURATION: 'user-middlewares-duration',
-  ...ERROR_REPORT,
+  USER_MIDDLEWARES_DURATION = 'user-middlewares-duration',
 }
 
-export const LOG_EVENTS = {
-  /** Event representing that userland middlewares are about to start */
-  USER_MIDDLEWARES_START: 'user-middlewares-start',
+export const enum HttpCacheLogFields {
+  /** The generated cache key for Memoization or Local caches */
+  KEY = 'key',
 
-  /** Event representing that userland middlewares just finished */
-  USER_MIDDLEWARES_FINISH: 'user-middlewares-finish',
+  /** The generated cache key for local cache with the segment added to it */
+  KEY_WITH_SEGMENT = 'key-with-segment',
+
+  /** The key that was just set on the cache */
+  KEY_SET = 'key-set',
+
+  /** The type of cache: 'disk', 'memory' or 'memoization' */
+  CACHE_TYPE = 'cache-type',
+
+  /** The etag for the current request used for caching decisions. Can be undefined. */
+  ETAG = 'etag',
+
+  /** The expiration time in seconds of the current cache entry (either just saved or fetched from the storage) */
+  EXPIRATION_TIME = 'expiration-time',
+
+  /** The http response type */
+  RESPONSE_TYPE = 'response-type',
+
+  /** The http response encoding */
+  RESPONSE_ENCONDING = 'response-encoding',
+
+  /** The forceMaxAge option on the incoming RequestConfig. This can be set to override no-cache or no-store headers */
+  FORCE_MAX_AGE = 'force-max-age',
+
+  /** The calculated max-age on the storage for the current response */
+  CALCULATED_MAX_AGE = 'calculated-max-age',
+
+  /** Content parsed from the incoming response headers */
+  AGE = 'age',
+  NO_CACHE = 'no-cache',
+  NO_STORE = 'no-store',
+  MAX_AGE = 'max-age',
+}
+
+export const enum HttpRetryLogFields {
+  /** The retry number that will be scheduled (e.g, 1, 2, meaning that is the first or second retry) */
+  RETRY_NUMBER = 'retry-number',
+
+  /** How much time in ms will be waited until this retry happens */
+  RETRY_IN = 'retry-in',
 }
--- src/tracing/Tags.ts
+++ src/tracing/Tags.ts
@@ -1,31 +1,54 @@
-import { Tags as opentracingTags } from 'opentracing'
+import { Tags as OpentracingTags } from 'opentracing'
+export { OpentracingTags }
 
-const VTEX_INCOMING_REQUEST_TAGS = {
-  VTEX_ACCOUNT: 'vtex.incoming.account',
-  VTEX_REQUEST_ID: 'vtex.request_id',
-  VTEX_WORKSPACE: 'vtex.incoming.workspace',
+/* tslint:disable:object-literal-sort-keys */
+
+export const enum VTEXIncomingRequestTags {
+  VTEX_ACCOUNT = 'vtex.incoming.account',
+  VTEX_REQUEST_ID = 'vtex.request_id',
+  VTEX_WORKSPACE = 'vtex.incoming.workspace',
 }
 
-const APP_TAGS = {
-  VTEX_APP_LINKED: 'app.linked',
-  VTEX_APP_NODE_ENV: 'app.node_env',
-  VTEX_APP_NODE_VTEX_API_VERSION: 'app.node_vtex_api_version',
-  VTEX_APP_PRODUCTION: 'app.production',
-  VTEX_APP_REGION: 'app.region',
-  VTEX_APP_VERSION: 'app.version',
-  VTEX_APP_WORKSPACE: 'app.workspace',
+export const enum AppTags {
+  VTEX_APP_LINKED = 'app.linked',
+  VTEX_APP_NODE_ENV = 'app.node_env',
+  VTEX_APP_NODE_VTEX_API_VERSION = 'app.node_vtex_api_version',
+  VTEX_APP_PRODUCTION = 'app.production',
+  VTEX_APP_REGION = 'app.region',
+  VTEX_APP_VERSION = 'app.version',
+  VTEX_APP_WORKSPACE = 'app.workspace',
 }
 
-export const USERLAND_TAGS = {
-  ...opentracingTags,
+export const enum CustomHttpTags {
+  HTTP_PATH = 'http.path',
+
+  /** Set to true when the client had no response, probably meaning that there was a client error */
+  HTTP_NO_RESPONSE = 'http.no_response',
+
+  /** The HTTP client name (e.g. Apps, Registry, Router) */
+  HTTP_CLIENT_NAME = 'http.client.name',
+
+  /**
+   * CACHE_ENABLED tags indicate if the Cache strategy is enabled
+   * for the specific request.
+   *
+   */
+  HTTP_MEMOIZATION_CACHE_ENABLED = 'http.cache.memoization.enabled',
+  HTTP_DISK_CACHE_ENABLED = 'http.cache.disk.enabled',
+  HTTP_MEMORY_CACHE_ENABLED = 'http.cache.memory.enabled',
+
+  /**
+   * CACHE_RESULT tags indicate the result for that cache strategy
+   * (HIT or MISS for example). Since there may be many layers
+   * of cache a ENABLED flag for a strategy may be 'true', but
+   * the RESULT for that strategy may not be present.
+   */
+  HTTP_MEMORY_CACHE_RESULT = 'http.cache.memory',
+  HTTP_MEMOIZATION_CACHE_RESULT = 'http.cache.memoization',
+  HTTP_DISK_CACHE_RESULT = 'http.cache.disk',
+  HTTP_ROUTER_CACHE_RESULT = 'http.cache.router',
 }
 
-export const Tags = {
-  HTTP_NO_RESPONSE: 'http.no_response',
-  HTTP_PATH: 'http.path',
-  HTTP_RETRY_COUNT: 'http.retry_count',
-  HTTP_ROUTER_CACHE: 'http.router_cache',
-  ...USERLAND_TAGS,
-  ...VTEX_INCOMING_REQUEST_TAGS,
-  ...APP_TAGS,
+export const UserlandTags = {
+  ...OpentracingTags,
 }
