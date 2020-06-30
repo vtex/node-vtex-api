4832fa06f9358a0ccd51e10ca93c880218532790
Bruno Abreu <bruno.abreu@vtex.com.br>
2020-06-30 12:57:36 -0300

    Remove fallback to x-vtex-credential as adminUserAuthToken

--- src/service/worker/runtime/http/middlewares/authTokens.ts
+++ src/service/worker/runtime/http/middlewares/authTokens.ts
@@ -12,7 +12,7 @@ export async function authTokens <
 > (ctx: ServiceContext<T, U, V>, next: () => Promise<void>) {
   const { vtex: { account } } = ctx
 
-  ctx.vtex.adminUserAuthToken = ctx.cookies.get(VTEX_ID_COOKIE_KEY) || ctx.headers[CREDENTIAL_HEADER]
+  ctx.vtex.adminUserAuthToken = ctx.cookies.get(VTEX_ID_COOKIE_KEY)
   ctx.vtex.storeUserAuthToken = ctx.cookies.get(`${VTEX_ID_COOKIE_KEY}_${account}`)
   ctx.vtex.janusEnv = ctx.cookies.get(JANUS_ENV_COOKIE_KEY)
 
