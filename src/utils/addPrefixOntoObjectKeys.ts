beea8e939c83abe77272ab3b5137fe8f11c64ceb
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-08-18 12:51:05 -0300

    Replace addPrefixOntoObjectKeys with cloneAndSanitizeHeaders
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/utils/addPrefixOntoObjectKeys.ts
+++
@@ -1,8 +0,0 @@
-export const addPrefixOntoObjectKeys = (prefix: string, obj: Record<string, string>) => {
-  const ret: Record<string, string> = {}
-  const entries = Object.entries(obj)
-  for (const [key, val] of entries) {
-    ret[`${prefix}.${key}`] = val
-  }
-  return ret
-}
