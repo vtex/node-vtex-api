8124522f08db0177087008b5ccbdbf4a9d3c56c7
Victor Elias <victorgelias@gmail.com>
2021-02-05 12:53:48 -0300

    Sort option keys alphabetically

--- src/HttpClient/agents.ts
+++ src/HttpClient/agents.ts
@@ -4,18 +4,18 @@ type HttpOptions = Omit<AgentHttpOptions, 'freeSocketTimeout' | 'keepAlive' | 'm
 
 export const createHttpAgent = (opts?: HttpOptions) => new HttpAgent({
   ...opts,
-  socketActiveTTL: 120 * 1000,
   freeSocketTimeout: 30 * 1000,
   keepAlive: true,
   maxFreeSockets: 256,
+  socketActiveTTL: 120 * 1000,
 })
 
 type HttpsOptions = Omit<AgentHttpsOptions, 'freeSocketTimeout' | 'keepAlive' | 'maxFreeSockets'>
 
 export const createHttpsAgent = (opts?: HttpsOptions) => new HttpsAgent({
   ...opts,
-  socketActiveTTL: 120 * 1000,
   freeSocketTimeout: 30 * 1000,
   keepAlive: true,
   maxFreeSockets: 256,
+  socketActiveTTL: 120 * 1000,
 })
