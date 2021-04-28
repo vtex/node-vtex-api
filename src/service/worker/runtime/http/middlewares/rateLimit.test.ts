b1f77ace742b75812d0cf1861cd9642c0a7cd5ac
Thiago Veras <thiago.veras.machado@gmail.com>
2021-04-28 12:46:33 -0300

    Finish rate limit tests

--- src/service/worker/runtime/http/middlewares/rateLimit.test.ts
+++ src/service/worker/runtime/http/middlewares/rateLimit.test.ts
@@ -1,46 +1,57 @@
-import { isType } from 'graphql'
+import Semaphore from 'semaphore-async-await'
 import TokenBucket from 'tokenbucket'
-import { TooManyRequestsError } from '../../../../../errors/TooManyRequestsError'
+import { ForbiddenError, TooManyRequestsError } from '../../../../../errors'
 import { ServiceContext } from '../../typings'
 import { createTokenBucket } from '../../utils/tokenBucket'
-import { perMinuteRateLimiter } from './rateLimit'
+import { concurrentRateLimiter, perMinuteRateLimiter } from './rateLimit'
 
 const nopCtx: ServiceContext = {} as ServiceContext
 const nopNext = async () => { return }
+const mutexNext = (mutex: Semaphore) => () => mutex.wait()
+const throwNext = () => { throw new ForbiddenError('Default message')}
 
-async function execRequests(requestsAmount: number, middleware: any) {
-  for (let i = 0; i < requestsAmount; i++){
+async function execRequests(requestsAmount: number, middleware: (ctx: ServiceContext, next: () => Promise<void>) => Promise<void>) {
+  for (let i = 0; i < requestsAmount; i++) {
     await middleware(nopCtx, nopNext)
   }
 }
 
-describe('rateLimitPerMinute', () => {
+function startRequest(middleware: (ctx: ServiceContext, next: () => Promise<any>) => Promise<void>, customNext?: () => Promise<void>) {
+  const mutex: Semaphore = new Semaphore(0)
+  const middlewarePromise = middleware(nopCtx, customNext ? customNext : mutexNext(mutex))
+  return { mutex, middlewarePromise }
+}
 
-  test('Test event per minute middleware', async () => {
+function getRequestsArray(
+  requestsAmount: number,
+  middleware: (ctx: ServiceContext, next: () => Promise<void>) => Promise<void>,
+  customNext?: () => Promise<void>
+): { mutexArr: Semaphore[], promisesArr: Array<Promise<void>> } {
+  const mutexArr: Semaphore[] = []
+  const promisesArr: Array<Promise<void>> = []
+  
+  for (let i = 0; i < requestsAmount; i++) {
+    const request = startRequest(middleware, customNext)
+    mutexArr.push(request.mutex)
+    promisesArr.push(request.middlewarePromise)
+  }
+  
+  return { mutexArr, promisesArr }
+}
+
+describe('Rate limit per minute', () => {
+
+  test('Test per minute middleware', async () => {
     // 2 * 1000 because bucket size is / 2 in order to not overflow amount of requests in 1 minute
     const globalRateLimitBucketPerMinute: TokenBucket | undefined = createTokenBucket(2 * 1000)
-    const perMinuteRateLimiterMiddlewareEvent = perMinuteRateLimiter(2 * 400, globalRateLimitBucketPerMinute)
-    
-    await expect(
-      execRequests(300, perMinuteRateLimiterMiddlewareEvent)
-    ).resolves.not.toThrowError(TooManyRequestsError)
-    
-    await expect(
-      execRequests(300, perMinuteRateLimiterMiddlewareEvent)
-    ).rejects.toThrowError(TooManyRequestsError)
-  })
+    const perMinuteRateLimiterMiddleware = perMinuteRateLimiter(2 * 400, globalRateLimitBucketPerMinute)
 
-
-  test('Test route per minute middleware', async () => {
-    const globalRateLimitBucketPerMinute: TokenBucket | undefined = createTokenBucket(2000)
-    const perMinuteRateLimiterMiddlewareRoute = perMinuteRateLimiter(2 * 400, globalRateLimitBucketPerMinute)
-    
     await expect(
-      execRequests(300, perMinuteRateLimiterMiddlewareRoute)
+      execRequests(300, perMinuteRateLimiterMiddleware)
     ).resolves.not.toThrowError(TooManyRequestsError)
-    
+
     await expect(
-      execRequests(600, perMinuteRateLimiterMiddlewareRoute)
+      execRequests(300, perMinuteRateLimiterMiddleware)
     ).rejects.toThrowError(TooManyRequestsError)
   })
 
@@ -61,7 +72,7 @@ describe('rateLimitPerMinute', () => {
     const globalRateLimitBucketPerMinute: TokenBucket | undefined = createTokenBucket(2 * 10000)
     const perMinuteRateLimiterMiddlewareRoute = perMinuteRateLimiter(2 * 2500, globalRateLimitBucketPerMinute)
     const perMinuteRateLimiterMiddlewareEvent = perMinuteRateLimiter(2 * 2500, globalRateLimitBucketPerMinute)
-    
+
     await expect(
       Promise.all([
         execRequests(2000, perMinuteRateLimiterMiddlewareRoute),
@@ -70,5 +81,82 @@ describe('rateLimitPerMinute', () => {
     ).resolves.not.toThrowError(TooManyRequestsError)
 
   })
-  
+
+})
+
+describe('Rate limit concurrent', () => {
+  test('Test concurrent middleware not throw 429', async () => {
+    const concurrentLimiterMiddleware = concurrentRateLimiter(5)
+
+    const firstHalfRequests = getRequestsArray(5, concurrentLimiterMiddleware)
+    for (let i = 0; i < 5; i++) {
+      firstHalfRequests.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(firstHalfRequests.promisesArr)
+    ).resolves.not.toThrowError(TooManyRequestsError)
+
+    const secondHalfRequests = getRequestsArray(5, concurrentLimiterMiddleware)
+    for (let i = 0; i < 5; i++) {
+      secondHalfRequests.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(secondHalfRequests.promisesArr)
+    ).resolves.not.toThrowError(TooManyRequestsError)
+  })
+
+  test('Test concurrent middleware throw 429', async () => {
+    const concurrentLimiterMiddleware = concurrentRateLimiter(5)
+    const firstPart = getRequestsArray(5, concurrentLimiterMiddleware)
+    const secondPart = getRequestsArray(5, concurrentLimiterMiddleware)
+
+    for (let i = 0; i < 5; i++) {
+      firstPart.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(firstPart.promisesArr)
+    ).resolves.not.toThrowError(TooManyRequestsError)
+
+    for (let i = 0; i < 5; i++) {
+      secondPart.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(secondPart.promisesArr)
+    ).rejects.toThrowError(TooManyRequestsError)
+
+    const thirdPart = getRequestsArray(5, concurrentLimiterMiddleware)
+    for (let i = 0; i < 5; i++) {
+      thirdPart.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(thirdPart.promisesArr)
+    ).resolves.not.toThrowError(TooManyRequestsError)
+  })
+
+  test('Test concurrent middleware throw error != 429', async () => {
+    const concurrentLimiterMiddleware = concurrentRateLimiter(5)
+
+    const firstHalfRequests = getRequestsArray(5, concurrentLimiterMiddleware, throwNext)
+    for (let i = 0; i < 5; i++) {
+      firstHalfRequests.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(firstHalfRequests.promisesArr)
+    ).rejects.toThrowError(ForbiddenError)
+
+    const secondHalfRequests = getRequestsArray(5, concurrentLimiterMiddleware)
+    for (let i = 0; i < 5; i++) {
+      secondHalfRequests.mutexArr[i].signal()
+    }
+
+    await expect(
+      Promise.all(secondHalfRequests.promisesArr)
+    ).resolves.not.toThrowError(TooManyRequestsError)
+  })
 })
