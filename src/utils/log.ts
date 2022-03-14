af0beca5f114d5bbb0c89ed3ddefe291841497c4
valentino-amadeus <valentino.sichinel@vtex.com.br>
2022-03-14 18:46:41 -0300

    add 'authorization' and 'vtexidclientautcookie' to sensitive_fields

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,6 @@
 import { cleanJson } from './json'
 
-const SENSITIVE_FIELDS = ['cookie', 'Cookie', 'vtexIdclientautcookie', 'error']
+const SENSITIVE_FIELDS = ['authorization', 'cookie', 'Cookie', 'vtexidclientautcookie', 'vtexIdclientautcookie', 'error']
 
 export const cleanLog = (log: any) => {
     return cleanJson(log, SENSITIVE_FIELDS)
