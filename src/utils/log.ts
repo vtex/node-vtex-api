05ca1613c5d3016d63849142174ce414e61332c0
valentino-amadeus <valentino.sichinel@vtex.com.br>
2022-04-14 11:23:55 -0300

    add missing sensitive fields

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,10 +1,16 @@
 import { cleanJson } from './json'
 
 const SENSITIVE_FIELDS = [
+    'auth',
     'authorization',
+    'authtoken',
     'cookie',
     'vtexidclientautcookie',
+    'proxy-authorization',
     'rawheaders',
+    'token',
+    'x-vtex-api-appkey',
+    'x-vtex-api-apptoken',
     'x-vtex-credential',
     'x-vtex-session',
 ]
