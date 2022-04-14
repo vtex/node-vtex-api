9bc822806d9df914b2efd25cac235b7cd086fbeb
valentino-amadeus <valentino.sichinel@vtex.com.br>
2022-04-14 11:21:26 -0300

    remove 'error' from log/sensitive_fields

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,6 @@
 import { cleanJson } from './json'
 
-const SENSITIVE_FIELDS = ['authorization', 'cookie', 'vtexidclientautcookie', 'rawheaders', 'x-vtex-credential', 'x-vtex-session', 'error']
+const SENSITIVE_FIELDS = ['authorization', 'cookie', 'vtexidclientautcookie', 'rawheaders', 'x-vtex-credential', 'x-vtex-session']
 
 export const cleanLog = (log: any) => {
     return cleanJson(log, SENSITIVE_FIELDS)
