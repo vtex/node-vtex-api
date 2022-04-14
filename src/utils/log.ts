bc9352aff0e29aaf0b38b9632c4ab9d2745819d9
valentino-amadeus <valentino.sichinel@vtex.com.br>
2022-04-14 11:22:31 -0300

    lint

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,13 @@
 import { cleanJson } from './json'
 
-const SENSITIVE_FIELDS = ['authorization', 'cookie', 'vtexidclientautcookie', 'rawheaders', 'x-vtex-credential', 'x-vtex-session']
+const SENSITIVE_FIELDS = [
+    'authorization',
+    'cookie',
+    'vtexidclientautcookie',
+    'rawheaders',
+    'x-vtex-credential',
+    'x-vtex-session',
+]
 
 export const cleanLog = (log: any) => {
     return cleanJson(log, SENSITIVE_FIELDS)
