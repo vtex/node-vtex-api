38fdee68b2a0165c71e9871347fc9b68cea3b930
arturpimentel <artur.oliveira@vtex.com>
2025-03-28 22:09:42 -0300

    Fix typings in HttpClient.middlewares.recorder
    Here, I also clause-guarded the error handling for an AxiosError instance

--- src/HttpClient/middlewares/recorder.ts
+++ src/HttpClient/middlewares/recorder.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios'
 import { MiddlewareContext } from '../typings'
 import {
   Recorder,
@@ -13,11 +14,15 @@ export const recorderMiddleware = (recorder: Recorder) =>
     try {
       await next()
       if (ctx.response) {
-        (recorder as Recorder).record(ctx.response.headers)
+        (recorder as Recorder).record(ctx.response.headers as any)
       }
     } catch (err) {
+      if (!(err instanceof AxiosError)) {
+        throw err
+      }
+      
       if (err.response && err.response.headers && err.response.status === 404) {
-        (recorder as Recorder).record(err.response.headers)
+        (recorder as Recorder).record(err.response.headers as any)
       }
       throw err
     }
