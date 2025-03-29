6a67a9cc9e984f57641c3f57d66fe4cc4b733ff5
arturpimentel <artur.oliveira@vtex.com>
2025-03-28 22:32:11 -0300

    Fix typings in service.worker.runtime.http.middlewares.error
    Only make operations with status codes if the error is an AxiosError

--- src/service/worker/runtime/http/middlewares/error.ts
+++ src/service/worker/runtime/http/middlewares/error.ts
@@ -1,3 +1,4 @@
+import { AxiosError } from 'axios'
 import { IOClients } from '../../../../../clients/IOClients'
 import { LINKED } from '../../../../../constants'
 import {
@@ -102,12 +103,13 @@ export async function error (ctx: ServiceContext, next: () => Promise<void>) {
     const err = cleanError(e)
 
     // Add response
-    ctx.status = e && e.status >= 400 && e.status <= 599
-      ? e.status
-      : ctx.status >= 500 && ctx.status <= 599
-        ? ctx.status
-        : 500
-
+    if(e instanceof AxiosError){
+      ctx.status = e?.status && e.status >= 400 && e.status <= 599
+        ? e.status
+        : ctx.status >= 500 && ctx.status <= 599
+          ? ctx.status
+          : 500
+    }
     // Do not generate etag for errors
     ctx.remove(META_HEADER)
     ctx.remove(ETAG_HEADER)
