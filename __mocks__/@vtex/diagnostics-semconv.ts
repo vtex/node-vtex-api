3b236741103e9b76424b5454ed915f9e09620875
Daniyel Rocha <daniyelnnr@gmail.com>
2025-09-10 14:18:55 -0300

    Update mock file
    Remove deprecated constant `ATTR_VTEX_OPERATION_ID` from diagnostics-semconv mock

--- __mocks__/@vtex/diagnostics-semconv.ts
+++ __mocks__/@vtex/diagnostics-semconv.ts
@@ -1,5 +1,4 @@
 // Mock para @vtex/diagnostics-semconv
-const ATTR_VTEX_OPERATION_ID = 'vtex.operation.id'
 const ATTR_VTEX_ACCOUNT_NAME = 'vtex.account.name'
 const ATTR_VTEX_IO_WORKSPACE_NAME = 'vtex_io.workspace.name'
 const ATTR_VTEX_IO_WORKSPACE_TYPE = 'vtex_io.workspace.type'
@@ -7,7 +6,6 @@ const ATTR_VTEX_IO_APP_ID = 'vtex_io.app.id'
 const ATTR_VTEX_IO_APP_AUTHOR_TYPE = 'vtex_io.app.author-type'
 
 export {
-  ATTR_VTEX_OPERATION_ID,
   ATTR_VTEX_ACCOUNT_NAME,
   ATTR_VTEX_IO_WORKSPACE_NAME,
   ATTR_VTEX_IO_WORKSPACE_TYPE,
