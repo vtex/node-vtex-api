2f1cf5ee8324d734dbd6007a1b92b7480f0677a2
Renato Almeida <renato.almeida@vtex.com>
2025-10-08 19:31:53 -0300

    Added new param state to translation parses

--- src/service/worker/runtime/graphql/utils/translations.ts
+++ src/service/worker/runtime/graphql/utils/translations.ts
@@ -4,12 +4,14 @@ import { MessagesLoaderV2 } from '../schema/messagesLoaderV2'
 
 export const CONTEXT_REGEX = /\(\(\((?<context>(.)*)\)\)\)/
 export const FROM_REGEX = /\<\<\<(?<from>(.)*)\>\>\>/
-export const CONTENT_REGEX = /\(\(\((?<context>(.)*)\)\)\)|\<\<\<(?<from>(.)*)\>\>\>/g
+export const STATE_REGEX = /\[\[\[(?<state>(.)*)\]\]\]/
+export const CONTENT_REGEX = /\(\(\((?<context>(.)*)\)\)\)|\<\<\<(?<from>(.)*)\>\>\>|\[\[\[(?<state>(.)*)\]\]\]/g
 
 export interface TranslatableMessageV2 {
   from?: string
   content: string
   context?: string
+  state?: 'original' | 'translated'
 }
 
 export type TranslationDirectiveType = 'translatableV2' | 'translateTo'
@@ -17,50 +19,53 @@ export type TranslationDirectiveType = 'translatableV2' | 'translateTo'
 export const parseTranslatableStringV2 = (rawMessage: string): TranslatableMessageV2 => {
   const context = rawMessage.match(CONTEXT_REGEX)?.groups?.context
   const from = rawMessage.match(FROM_REGEX)?.groups?.from
+  const state = rawMessage.match(STATE_REGEX)?.groups?.state
   const content = rawMessage.replace(CONTENT_REGEX, '')
 
   return {
     content: content?.trim(),
     context: context?.trim(),
     from: from?.trim(),
+    state: state?.trim() as 'original' | 'translated' | undefined,
   }
 }
 
-export const formatTranslatableStringV2 = ({ from, content, context }: TranslatableMessageV2): string =>
-  `${content} ${context ? `(((${context})))` : ''} ${from ? `<<<${from}>>>` : ''}`
+export const formatTranslatableStringV2 = ({ from, content, context, state }: TranslatableMessageV2): string =>
+  `${content} ${context ? `(((${context})))` : ''} ${from ? `<<<${from}>>>` : ''} ${state ? `[[[${state}]]]` : ''}`
 
-export const handleSingleString = (
-  ctx: IOContext,
-  loader: MessagesLoaderV2,
-  behavior: Behavior,
-  directiveName: TranslationDirectiveType
-) => async (rawMessage: string | null) => {
-  // Messages only know how to process non empty strings.
-  if (rawMessage == null) {
-    return rawMessage
+export const handleSingleString =
+  (ctx: IOContext, loader: MessagesLoaderV2, behavior: Behavior, directiveName: TranslationDirectiveType) =>
+  async (rawMessage: string | null) => {
+    // Messages only know how to process non empty strings.
+    if (rawMessage == null) {
+      return rawMessage
+    }
+
+    const { content, context, from: maybeFrom, state } = parseTranslatableStringV2(rawMessage)
+    const { binding, tenant } = ctx
+
+    if (content == null) {
+      throw new Error(
+        `@${directiveName} directive needs a content to translate, but received ${JSON.stringify(rawMessage)}`
+      )
+    }
+
+    const from = maybeFrom || binding?.locale || tenant?.locale
+
+    if (from == null) {
+      throw new Error(
+        `@${directiveName} directive needs a source language to translate from. You can do this by either setting ${'`ctx.vtex.tenant`'} variable, call this app with the header ${'`x-vtex-tenant`'} or format the string with the ${'`formatTranslatableStringV2`'} function with the ${'`from`'} option set`
+      )
+    }
+
+    if (state === 'translated') {
+      return content
+    }
+
+    return loader.load({
+      behavior,
+      content,
+      context,
+      from,
+    })
   }
-
-  const { content, context, from: maybeFrom } = parseTranslatableStringV2(rawMessage)
-  const { binding, tenant } = ctx
-
-  if (content == null) {
-    throw new Error(
-      `@${directiveName} directive needs a content to translate, but received ${JSON.stringify(rawMessage)}`
-    )
-  }
-
-  const from = maybeFrom || binding?.locale || tenant?.locale
-
-  if (from == null) {
-    throw new Error(
-      `@${directiveName} directive needs a source language to translate from. You can do this by either setting ${'`ctx.vtex.tenant`'} variable, call this app with the header ${'`x-vtex-tenant`'} or format the string with the ${'`formatTranslatableStringV2`'} function with the ${'`from`'} option set`
-    )
-  }
-
-  return loader.load({
-    behavior,
-    content,
-    context,
-    from,
-  })
-}
